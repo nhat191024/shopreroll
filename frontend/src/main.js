143ea7a8ef597ebf7ebfee07ecda67b64c2499af
nhat191024 <richberchannel01@gmail.com>
2024-07-28 14:31:15 +0700

    chore: Update main.js to use router and axios for API calls

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
+import router from './router/index';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(router)
+.use(VueAxios, axios)
+.mount('#app')
