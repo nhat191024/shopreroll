0a1cc9bd308b8c43046e5e20577fdac4008ab220
nhat191024 <richberchannel01@gmail.com>
2024-07-29 11:40:42 +0700

    chore: Update PrimeVue configuration and dependencies

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
 import router from './router/index';
 import axios from 'axios'
-import VueAxios from 'vue-axios'
+import PrimeVue from "primevue/config";
+import pre from '../presets/aura'
 
 import './style.css'
+import 'primeicons/primeicons.css'
 import App from './App.vue'
 
 createApp(App)
-.use(router)
-.use(VueAxios, axios)
-.mount('#app')
+    .use(PrimeVue, {
+        unstyled: true,
+        pt: pre,
+    })
+    .use(router)
+    .mount('#app');
