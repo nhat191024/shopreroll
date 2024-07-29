10bea481df04c00315fcbfc5eedb3e4c13586944
nhat191024 <richberchannel01@gmail.com>
2024-07-29 11:39:46 +0700

    chore: Update Tailwind CSS configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,16 @@
+const primeui = require('tailwindcss-primeui');
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: [
     "./index.html",
+    "./presets/**/*.{js,vue,ts}",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
+  plugins: [primeui],
   theme: {
-    extend: {},
-  },
-  plugins: [
-    require('tailwindcss-primeui')
-  ]
+    extend: {
+    },
+  }
 }
 
