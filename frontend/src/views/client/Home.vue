e82cf3a32fb53bcaa345566d1bd6e83a4fcac65e
nhat191024 <richberchannel01@gmail.com>
2024-08-08 07:07:30 +0700

    refactor: Update home page layout in Home.vue

--- frontend/src/views/client/Home.vue
+++ frontend/src/views/client/Home.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>
-        This is the home page
+    <!-- demo home -->
+    <div class=" relative text-orange-500 z-10">
+        <div class="h-dvh">this is home 1</div>
+        <div class="h-dvh">this is home 2</div>
     </div>
 </template>
