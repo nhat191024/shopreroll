a271a38bdc28b421c66ac4af717dcc8ce5913b59
nhat191024 <richberchannel01@gmail.com>
2024-08-08 07:07:39 +0700

    feat: Add Client layout component with navbar, footer, and router view

---
+++ frontend/src/layouts/Client.vue
@@ -0,0 +1,10 @@
+<template>
+    <Navbar></Navbar>
+    <img src="/bg.webp" alt="" class=" fixed h-dvh w-dvw z-0">
+    <router-view></router-view>
+    <Footer></Footer>
+</template>
+<script setup>
+import Navbar from '@/components/client/NavBar.vue';
+import Footer from '@/components/client/Footer.vue';
+</script>
