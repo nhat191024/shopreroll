73c4d9b7a7a3a895f90f3f34289bc411416b6074
nhat191024 <richberchannel01@gmail.com>
2024-08-29 00:43:42 +0700

    refactor: Update tailwind.config.js to add custom text shadow utilities

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,28 @@ export default {
     "./presets/**/*.{js,vue,ts}",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  plugins: [primeui]
+  theme: {
+    extend: {
+      textShadow: {
+        'custom': 'rgb(126, 86, 63) 0px 0px 10px, rgb(40, 36, 23) 0px 0px 20px, rgb(41, 36, 24) 0px 0px 40px !important',
+        'xl': '2px 2px 2px rgba(0, 0, 0, 0.5)',
+      },
+    },
+  },
+  plugins: [
+    primeui,
+    function({ addUtilities }) {
+      const newUtilities = {
+        '.text-shadow-custom': {
+          textShadow: 'rgb(126, 86, 63) 0px 0px 10px, rgb(40, 36, 23) 0px 0px 20px, rgb(41, 36, 24) 0px 0px 40px !important',
+        },
+        'text-shadow-xl': {
+          textShadow: '2px 2px 2px rgba(0, 0, 0, 0.5)',
+        },
+      }
+
+      addUtilities(newUtilities, ['responsive', 'hover'])
+    }
+  ],
 }
 
