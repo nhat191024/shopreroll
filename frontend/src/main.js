4943550d0bfce1961d0228ed909d75304e47a321
nhat191024 <richberchannel01@gmail.com>
2024-09-01 20:39:05 +0700

    feat: Add vue-recaptcha plugin for form validation

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import { VueRecaptchaPlugin, useRecaptchaProvider } from 'vue-recaptcha'
+import { createHead } from '@unhead/vue'
 import router from './router/index';
 import axios from 'axios'
 import PrimeVue from "primevue/config";
@@ -8,10 +10,17 @@ import './style.css'
 import 'primeicons/primeicons.css'
 import App from './App.vue'
 
+const head = createHead()
+
 createApp(App)
+    .use(head)
     .use(PrimeVue, {
         unstyled: true,
         pt: pre,
+    })
+    .use(VueRecaptchaPlugin, {
+        v2SiteKey: import.meta.env.VITE_RECAPTCHA_V2_SITE,
+        v3SiteKey: import.meta.env.VITE_RECAPTCHA_V3_SITE,
     })
     .use(router)
     .mount('#app');
