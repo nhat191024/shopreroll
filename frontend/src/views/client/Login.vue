ac0bb310c05deae9d4dd72bc315e2049098281af
nhat191024 <richberchannel01@gmail.com>
2024-09-04 07:19:13 +0700

    refactor: Improve login form styling and error handling

--- frontend/src/views/client/Login.vue
+++ frontend/src/views/client/Login.vue
@@ -2,31 +2,32 @@
     <div class="h-dvh relative z-10 flex flex-col items-center py-1">
         <img src="/title.png" alt="title" width="100">
         <Form @submit="onSubmit"
-            class="w-11/12 md:w-3/5 xl:w-1/5 h-fit mt-2 bg-white border border-black border-1 rounded-xl flex flex-col gap-5 items-center">
-            <p class="mt-6 mb-3 text-3xl md:text-4xl md:font-medium text-primary-50 text-shadow-custom">Đăng Nhập</p>
+            class="relative w-11/12 md:w-3/5 xl:w-1/5 h-fit mt-2 rounded-xl flex flex-col gap-5 items-center">
+            <div class="absolute z-0 w-full h-full bg-white opacity-85 rounded-xl"></div>
+            <p class="z-10 mt-6 mb-3 text-3xl md:text-4xl md:font-medium text-primary-50 text-shadow-custom">Đăng Nhập</p>
             <div>
                 <FloatLabel>
                     <InputText v-model="username"/>
                     <label for="username">Tên đăng nhập</label>
                 </FloatLabel>
-                <p class="text-red-700 text-center mt-1">{{ usernameError }}</p>
+                <p class="text-red-700 text-center mt-1 z-10">{{ usernameError }}</p>
             </div>
             <div class="mt-1 flex flex-col items-center">
                 <FloatLabel>
                     <InputText id="password" v-model="password" type="password"/>
                     <label for="password">Mật khẩu</label>
                 </FloatLabel>
-                <p class="text-red-700 text-center mt-1">{{ passwordError }}</p>
+                <p class="text-red-700 text-center mt-1 z-10">{{ passwordError }}</p>
             </div>
             <div class="flex flex-col items-center">
-                <Checkbox v-model="captcha" />
-                <p class="text-red-700 text-center mt-1 px-3">{{ captchaError }}</p>
+                <Checkbox v-model="captcha" class="z-10"/>
+                <p class="text-red-700 text-center mt-1 px-3 z-10">{{ captchaError }}</p>
             </div>
             <div class="flex gap-5">
                 <Button label="Đăng nhập" severity="info" type="submit"></Button>
                 <Button label="Đăng ký" severity="info" as="router-link" to="/register"></Button>
             </div>
-            <a href="" class="mb-3">Quên mật khẩu? Lấy lại</a>
+            <a href="" class="mb-3 z-10">Quên mật khẩu? Lấy lại</a>
         </Form>
     </div>
 </template>
