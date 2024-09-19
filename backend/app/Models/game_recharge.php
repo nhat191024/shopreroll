2c059f8e2c38ce8252f430e31b6de944cb652cd7
nhat191024 <richberchannel01@gmail.com>
2024-09-19 23:12:23 +0700

    feat: Add game_recharge model and migration

---
+++ backend/app/Models/game_recharge.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class game_recharge extends Model
+{
+    protected $table = 'game_recharges';
+    protected $fillable = [
+        'name',
+        'tutorial',
+        'id_youtube',
+        'image',
+        'status',
+    ];
+}
