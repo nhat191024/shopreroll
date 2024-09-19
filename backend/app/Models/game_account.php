84b545d43dea1d51f6b6d41d484ee58270ba5fd2
nhat191024 <richberchannel01@gmail.com>
2024-09-19 23:14:42 +0700

    feat: Add game_account model and migration

---
+++ backend/app/Models/game_account.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class game_account extends Model
+{
+    protected $table = 'game_accounts';
+    protected $fillable = [
+        'creator_id',
+        'game_category_id',
+        'title',
+        'username',
+        'password',
+        'AR',
+        'server',
+        'price_in',
+        'price_out',
+        'description',
+        'status',
+    ];
+}
---
+++ backend/database/migrations/2024_09_19_154626_create_game_accounts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('game_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('creator_id'); 
+            $table->unsignedBigInteger('game_category_id');
+            $table->string('title');
+            $table->string('username');
+            $table->string('password');
+            $table->string('AR');
+            $table->string('server');
+            $table->integer('price_in');
+            $table->integer('price_out');
+            $table->text('description');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+
+            $table->foreign('creator_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('game_category_id')->references('id')->on('game_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('game_accounts');
+    }
+};
