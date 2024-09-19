1b9581aa9d5db3219f9cb2eb7de1bf4c5fdcb65b
nhat191024 <richberchannel01@gmail.com>
2024-09-19 23:15:18 +0700

    feat: Add game model and migration

---
+++ backend/app/Models/game.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class game_category extends Model
+{
+    protected $table = 'game';
+    protected $fillable = [
+        'name',
+        'status',
+    ];
+}
