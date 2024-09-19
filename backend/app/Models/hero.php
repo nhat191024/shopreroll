a6035b599cc1c9f9a30c12cebb716c6737cf83df
nhat191024 <richberchannel01@gmail.com>
2024-09-19 23:12:56 +0700

    feat: Add hero model and migration

---
+++ backend/app/Models/hero.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class hero extends Model
+{
+    protected $table = 'heroes';
+    protected $fillable = [
+        'name',
+        'price',
+        'status',
+    ];
+}
---
+++ backend/database/migrations/2024_09_19_154233_create_heroes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('heroes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('game_id');
+            $table->string('name');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+
+            $table->foreign('game_id')->references('id')->on('games');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('heroes');
+    }
+};
