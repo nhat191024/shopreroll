04d68b2619ebbb9171f67b6975e2762dcc6e991c
nhat191024 <richberchannel01@gmail.com>
2024-09-19 23:14:29 +0700

    feat: Add balance_recharge_card_bill model and migration

---
+++ backend/app/Models/balance_recharge_card_bill.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class balance_recharge_card_bill extends Model
+{
+    protected $table = 'balance_recharge_card_bills';
+    protected $fillable = [
+        'user_id',
+        'card_number',
+        'card_serial',
+        'mobil_carrier',
+        'amount_fake',
+        'amount_real',
+        'balance_added',
+        'status',
+    ];
+}
