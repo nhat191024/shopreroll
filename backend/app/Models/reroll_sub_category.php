36ea63234bf3dae4d4dbf73cd68010fa6a870a4e
nhat191024 <richberchannel01@gmail.com>
2024-09-20 14:49:33 +0700

    Refactor: Rename sub_reroll_category to reroll_sub_category

--- backend/app/Models/reroll_sub_category.php
+++ backend/app/Models/reroll_sub_category.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class sub_reroll_category extends Model
+class reroll_sub_category extends Model
 {
     protected $table = 'reroll_sub_categories';
     protected $fillable = [
