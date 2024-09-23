c0d37005a01ad7cc49770ab84985d7d943b6e34b
nhat191024 <richberchannel01@gmail.com>
2024-09-23 23:42:34 +0700

    Refactor: Rename 'bank_name' column to 'bank' and 'description' column to 'note' in 'balance_recharge_bank_bill' model

--- backend/app/Models/balance_recharge_bank_bill.php
+++ backend/app/Models/balance_recharge_bank_bill.php
@@ -10,11 +10,11 @@ class balance_recharge_bank_bill extends Model
     protected $table = 'balance_recharge_bank_bills';
     protected $fillable = [
         'user_id',
-        'bank_name',
+        'bank',
         'amount',
         'balance_before',
         'balance_after',
-        'description',
+        'note',
         'status',
     ];
 
