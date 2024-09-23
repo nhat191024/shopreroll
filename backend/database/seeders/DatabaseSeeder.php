80b74ab87485ab0d0551d8116f3c9597fcd8b235
nhat191024 <richberchannel01@gmail.com>
2024-09-24 00:33:17 +0700

    Refactor: Seed database with data from JSON file

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,8 +3,29 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\game_recharge;
+use App\Models\recharge_package;
+use App\Models\recharge_bill;
+use App\Models\reroll_category;
+use App\Models\reroll_sub_category;
+use App\Models\reroll_package;
+use App\Models\reroll_key;
+use App\Models\reroll_bill;
+use App\Models\game;
+use App\Models\game_category;
+use App\Models\hero;
+use App\Models\weapon;
+use App\Models\game_account;
+use App\Models\account_hero;
+use App\Models\account_weapon;
+use App\Models\account_image;
+use App\Models\account_bill;
+use App\Models\balance_recharge_card_bill;
+use App\Models\balance_recharge_bank_bill;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +34,59 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $jsonFilePath = "./database/seeders/data.json";
+        $jsonContent = file_get_contents($jsonFilePath);
+        $dataArray = json_decode($jsonContent, true);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($dataArray['user'] as $data) {
+            User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'username' => $data['username'],
+                'password' => Hash::make($data['password']),
+                'phone' => $data['phone'],
+                'role' => $data['role'],
+                'balance' => $data['balance'],
+                'avatar' => $data['avatar'],
+            ]);
+        }
+
+        foreach ($dataArray['game_recharge'] as $data) { game_recharge::create($data); }
+
+        foreach ($dataArray['recharge_package'] as $data) { recharge_package::create($data); }
+
+        foreach ($dataArray['recharge_bill'] as $data) { recharge_bill::create($data); }
+
+        foreach ($dataArray['reroll_category'] as $data) { reroll_category::create($data); }
+
+        foreach ($dataArray['reroll_sub_category'] as $data) { reroll_sub_category::create($data); }
+
+        foreach ($dataArray['reroll_package'] as $data) { reroll_package::create($data); }
+
+        foreach ($dataArray['reroll_key'] as $data) { reroll_key::create($data); }
+
+        foreach ($dataArray['reroll_bill'] as $data) { reroll_bill::create($data); }
+
+        foreach ($dataArray['game'] as $data) { game::create($data); }
+
+        foreach ($dataArray['game_category'] as $data) { game_category::create($data); }
+
+        foreach ($dataArray['hero'] as $data) { hero::create($data); }
+
+        foreach ($dataArray['weapon'] as $data) { weapon::create($data); }
+
+        foreach ($dataArray['game_account'] as $data) { game_account::create($data); }
+
+        foreach ($dataArray['account_hero'] as $data) { account_hero::create($data); }
+
+        foreach ($dataArray['account_weapon'] as $data) { account_weapon::create($data); }
+
+        foreach ($dataArray['account_image'] as $data) { account_image::create($data); }
+
+        foreach ($dataArray['account_bill'] as $data) { account_bill::create($data); }
+
+        foreach ($dataArray['balance_recharge_card_bill'] as $data) { balance_recharge_card_bill::create($data); }
+
+        foreach ($dataArray['balance_recharge_bank_bill'] as $data) { balance_recharge_bank_bill::create($data); }
     }
 }
