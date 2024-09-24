bae1580128dd2ce25f888202ad1713000fed532d
nhat191024 <richberchannel01@gmail.com>
2024-09-24 22:19:53 +0700

    Refactor: Rename reroll_category.php to RerollCategory.php and update class name

--- backend/app/Models/RerollCategory.php
+++ backend/app/Models/RerollCategory.php
@@ -1,11 +1,9 @@
 <?php
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class reroll_category extends Model
+class RerollCategory extends Model
 {
     protected $table = 'reroll_categories';
     protected $fillable = [
@@ -17,6 +15,6 @@ class reroll_category extends Model
 
     public function SubRerollCategory()
     {
-        return $this->hasMany(reroll_sub_category::class);
+        return $this->hasMany(RerollSubCategory::class);
     }
 }
