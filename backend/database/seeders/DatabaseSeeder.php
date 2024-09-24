d723050ab494637be8379455039db4255ea3e33d
nhat191024 <richberchannel01@gmail.com>
2024-09-24 22:20:11 +0700

    Refactor: Update class names in DatabaseSeeder.php

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,25 +3,25 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\game_recharge;
-use App\Models\recharge_package;
-use App\Models\recharge_bill;
-use App\Models\reroll_category;
-use App\Models\reroll_sub_category;
-use App\Models\reroll_package;
-use App\Models\reroll_key;
-use App\Models\reroll_bill;
-use App\Models\game;
-use App\Models\game_category;
-use App\Models\hero;
-use App\Models\weapon;
-use App\Models\game_account;
-use App\Models\account_hero;
-use App\Models\account_weapon;
-use App\Models\account_image;
-use App\Models\account_bill;
-use App\Models\balance_recharge_card_bill;
-use App\Models\balance_recharge_bank_bill;
+use App\Models\GameRecharge;
+use App\Models\RechargePackage;
+use App\Models\RechargeBill;
+use App\Models\RerollCategory;
+use App\Models\RerollSubCategory;
+use App\Models\RerollPackage;
+use App\Models\RerollKey;
+use App\Models\RerollBill;
+use App\Models\Game;
+use App\Models\GameCategory;
+use App\Models\Hero;
+use App\Models\Weapon;
+use App\Models\GameAccount;
+use App\Models\AccountHero;
+use App\Models\AccountWeapon;
+use App\Models\AccountImage;
+use App\Models\AccountBill;
+use App\Models\BalanceRechargeCardBill;
+use App\Models\BalanceRechargeBankBill;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -51,42 +51,42 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
-        foreach ($dataArray['game_recharge'] as $data) { game_recharge::create($data); }
+        foreach ($dataArray['game_recharge'] as $data) { GameRecharge::create($data); }
 
-        foreach ($dataArray['recharge_package'] as $data) { recharge_package::create($data); }
+        foreach ($dataArray['recharge_package'] as $data) { RechargePackage::create($data); }
 
-        foreach ($dataArray['recharge_bill'] as $data) { recharge_bill::create($data); }
+        foreach ($dataArray['recharge_bill'] as $data) { RechargeBill::create($data); }
 
-        foreach ($dataArray['reroll_category'] as $data) { reroll_category::create($data); }
+        foreach ($dataArray['reroll_category'] as $data) { RerollCategory::create($data); }
 
-        foreach ($dataArray['reroll_sub_category'] as $data) { reroll_sub_category::create($data); }
+        foreach ($dataArray['reroll_sub_category'] as $data) { RerollSubCategory::create($data); }
 
-        foreach ($dataArray['reroll_package'] as $data) { reroll_package::create($data); }
+        foreach ($dataArray['reroll_package'] as $data) { RerollPackage::create($data); }
 
-        foreach ($dataArray['reroll_key'] as $data) { reroll_key::create($data); }
+        foreach ($dataArray['reroll_key'] as $data) { RerollKey::create($data); }
 
-        foreach ($dataArray['reroll_bill'] as $data) { reroll_bill::create($data); }
+        foreach ($dataArray['reroll_bill'] as $data) { RerollBill::create($data); }
 
-        foreach ($dataArray['game'] as $data) { game::create($data); }
+        foreach ($dataArray['game'] as $data) { Game::create($data); }
 
-        foreach ($dataArray['game_category'] as $data) { game_category::create($data); }
+        foreach ($dataArray['game_category'] as $data) { GameCategory::create($data); }
 
-        foreach ($dataArray['hero'] as $data) { hero::create($data); }
+        foreach ($dataArray['hero'] as $data) { Hero::create($data); }
 
-        foreach ($dataArray['weapon'] as $data) { weapon::create($data); }
+        foreach ($dataArray['weapon'] as $data) { Weapon::create($data); }
 
-        foreach ($dataArray['game_account'] as $data) { game_account::create($data); }
+        foreach ($dataArray['game_account'] as $data) { GameAccount::create($data); }
 
-        foreach ($dataArray['account_hero'] as $data) { account_hero::create($data); }
+        foreach ($dataArray['account_hero'] as $data) { AccountHero::create($data); }
 
-        foreach ($dataArray['account_weapon'] as $data) { account_weapon::create($data); }
+        foreach ($dataArray['account_weapon'] as $data) { AccountWeapon::create($data); }
 
-        foreach ($dataArray['account_image'] as $data) { account_image::create($data); }
+        foreach ($dataArray['account_image'] as $data) { AccountImage::create($data); }
 
-        foreach ($dataArray['account_bill'] as $data) { account_bill::create($data); }
+        foreach ($dataArray['account_bill'] as $data) { AccountBill::create($data); }
 
-        foreach ($dataArray['balance_recharge_card_bill'] as $data) { balance_recharge_card_bill::create($data); }
+        foreach ($dataArray['balance_recharge_card_bill'] as $data) { BalanceRechargeCardBill::create($data); }
 
-        foreach ($dataArray['balance_recharge_bank_bill'] as $data) { balance_recharge_bank_bill::create($data); }
+        foreach ($dataArray['balance_recharge_bank_bill'] as $data) { BalanceRechargeBankBill::create($data); }
     }
 }
