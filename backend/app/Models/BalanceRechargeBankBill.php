f16dfbc66f3267ebfccd40b78c818d386c7ff501
nhat191024 <richberchannel01@gmail.com>
2024-09-24 22:13:29 +0700

    Refactor: Rename balance_recharge_bank_bill.php to BalanceRechargeBankBill.php and update class name

--- backend/app/Models/BalanceRechargeBankBill.php
+++ backend/app/Models/BalanceRechargeBankBill.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class balance_recharge_bank_bill extends Model
+class BalanceRechargeBankBill extends Model
 {
     protected $table = 'balance_recharge_bank_bills';
     protected $fillable = [
