9ff5b4625735fdbdfbf25c4c155f42f6c59f663f
nhat191024 <richberchannel01@gmail.com>
2024-09-28 23:08:56 +0700

    Refactor: edit_rerollsubcategory.blade.php to improve code readability and maintainability

--- backend/resources/views/admin/RerollSubCategory/edit_rerollsubcategory.blade.php
+++ backend/resources/views/admin/RerollSubCategory/edit_rerollsubcategory.blade.php
@@ -13,57 +13,64 @@
             <div class="card-body">
                 <div class="table-responsive">
                     <form action="{{ route('admin.reroll_sub_category.edit') }}" method="post" enctype="multipart/form-data">
-                            @csrf
-                            <label for="">Tên Reroll Category</label>
-                            <input maxlength="255" required type="text" class="form-control" id="productName"
-                                aria-describedby="" name="name" value="{{ old('name', $rerollSubCategoryInfo->name ?? '') }}"
-                                placeholder="Nhập tên sản phẩm">
-                        </div>
-                        <div class="form-group">
-                            <label for="">Hướng dẫn</label>
-                            <input required type="text" class="form-control" id="productDescription" aria-describedby=""
-                                name="tutorial" value="{{ old('tutorial', $rerollSubCategoryInfo->tutorial ?? '') }}"
-                                placeholder="Nhập tên sản phẩm">
-                        </div>
-                        <div class="form-group">
-                            <label for="">Mã Youtube</label>
-                            <input type="text" class="form-control" id="productDescription" aria-describedby=""
-                                name="id_youtube" value="{{ old('id_youtube', $rerollSubCategoryInfo->id_youtube ?? '') }}"
-                                placeholder="Nhập tên sản phẩm">
-                        </div>
-                        <div class="form-group">
-                            <label for="">File download link</label>
-                            <input type="text" class="form-control" id="productDescription" aria-describedby=""
-                                name="file_download_link" value="{{ old('file_download_link', $rerollSubCategoryInfo->file_download_link ?? '') }}"
-                                placeholder="Nhập tên sản phẩm">
-                        </div>
-                        <div class="form-group">
-                            <label for="categorySelect">Chọn danh mục</label>
-                            <select class="form-control" id="categorySelect" name="reroll_category_id" required>
-                                @foreach ($allRerollCategories as $id => $name)
-                                    <option value="{{ $id }}" {{ old('reroll_category_id', $idRerollSubCategory) == $rerollSubCategoryInfo->reroll_category_id ? 'selected' : '' }}>
-                                        {{ $name }}
-                                    </option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <label for="imageFrame">Ảnh sản phẩm</label>
-                        <div class="image-frame" style="border: 2px dashed #ccc; padding: 10px; text-align: center; cursor: pointer;">
-                            @if($rerollSubCategoryInfo->image)
-                                <img src="{{ asset('image/thumb/' . $rerollSubCategoryInfo['image']) }}" alt="Current Image" id="currentImage" style="max-width: 150px; max-height: 150px;"/>
-                            @else
-                                <span>Chưa có ảnh</span>
-                            @endif
-                            <input type="file" accept="image/*" id="imageInput" name="image" style="display: none;">
-                        </div>
-                        <input type="hidden" value="{{ $rerollSubCategoryInfo->id }}" name="id">
-                        <a class="btn btn-primary mt-4" onclick="history.back()">Quay lại</a>
-                        <button id="saveAdd" class="btn btn-success mt-4" type="submit">Lưu</button>
-                    </form>
-
+                        @csrf
+                        <label for="">Tên Reroll Category</label>
+                        <input maxlength="255" required type="text" class="form-control" id="productName"
+                            aria-describedby="" name="name" value="{{ old('name', $rerollSubCategoryInfo->name ?? '') }}"
+                            placeholder="Nhập tên sản phẩm">
                 </div>
+                <div class="form-group">
+                    <label for="">Hướng dẫn</label>
+                    <input required type="text" class="form-control" id="productDescription" aria-describedby=""
+                        name="tutorial" value="{{ old('tutorial', $rerollSubCategoryInfo->tutorial ?? '') }}"
+                        placeholder="Nhập tên sản phẩm">
+                </div>
+                <div class="form-group">
+                    <label for="">Mã Youtube</label>
+                    <input type="text" class="form-control" id="productDescription" aria-describedby="" name="id_youtube"
+                        value="{{ old('id_youtube', $rerollSubCategoryInfo->id_youtube ?? '') }}"
+                        placeholder="Nhập tên sản phẩm">
+                </div>
+                <div class="form-group">
+                    <label for="">File download link</label>
+                    <input type="text" class="form-control" id="productDescription" aria-describedby=""
+                        name="file_download_link"
+                        value="{{ old('file_download_link', $rerollSubCategoryInfo->file_download_link ?? '') }}"
+                        placeholder="Nhập tên sản phẩm">
+                </div>
+                <div class="form-group">
+                    <label for="categorySelect">Chọn danh mục</label>
+                    <select class="form-control" id="categorySelect" name="reroll_category_id" required>
+                        @foreach ($rerollCategories as $id => $name)
+                            @if ($id == $rerollSubCategoryInfo->reroll_category_id)
+                                <option value="{{ $id }}" selected>{{ $name }}</option>
+                            @else
+                                <option value="{{ $id }}">{{ $name }}</option>
+                            @endif
+                        @endforeach
+                        {{-- <option value="">Chọn danh mục</option>
+                        <option value="1" selected> oauidwyaiohlw</option> --}}
+                    </select>
+                </div>
+                <label for="imageFrame">Ảnh sản phẩm</label>
+                <div class="image-frame"
+                    style="border: 2px dashed #ccc; padding: 10px; text-align: center; cursor: pointer;">
+                    @if ($rerollSubCategoryInfo->image)
+                        <img src="{{ asset('image/thumb/' . $rerollSubCategoryInfo['image']) }}" alt="Current Image"
+                            id="currentImage" style="max-width: 150px; max-height: 150px;" />
+                    @else
+                        <span>Chưa có ảnh</span>
+                    @endif
+                    <input type="file" accept="image/*" id="imageInput" name="image" style="display: none;">
+                </div>
+                <input type="hidden" value="{{ $rerollSubCategoryInfo->id }}" name="id">
+                <a class="btn btn-primary mt-4" onclick="history.back()">Quay lại</a>
+                <button id="saveAdd" class="btn btn-success mt-4" type="submit">Lưu</button>
+                </form>
+
             </div>
         </div>
+    </div>
 
     </div>
     <!-- /.container-fluid -->
