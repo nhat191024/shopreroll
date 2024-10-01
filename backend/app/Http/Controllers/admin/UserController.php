9ec36359e5a5bae98a6f0357261123eae1bf71e7
huylee47 <huyw300@gmail.com>
2024-10-01 09:56:43 +0700

    Adjust & create : update role & add/sub balance controller

--- backend/app/Http/Controllers/admin/UserController.php
+++ backend/app/Http/Controllers/admin/UserController.php
@@ -22,69 +22,18 @@ class UserController extends Controller
         $allUser = $this->userService->getAll();
         return view('admin.user.user', compact('allUser'));
     }
-
-    // public function showAddUser()
-    // {
-    //     $allCategory = $this->categoryService->getAll();
-    //     return view('admin.food.add_food', compact('allCategory'));
-    // }
-
-    // public function addUser(Request $request)
-    // {
-    //     $request->validate([
-    //         'category_id' => 'required',
-    //         'food_name' => 'required',
-    //         'food_price' => 'required',
-    //         'food_image' => 'required',
-    //     ]);
-    //     $categoryId = $request->category_id;
-    //     $foodName = $request->food_name;
-    //     $foodPrice = $request->food_price;
-    //     $imageName = time() . '_' . $request->food_image->getClientOriginalName();
-    //     // Public Folder
-    //     $request->food_image->move(public_path('img'), $imageName);
-    //     $this->foodService->add($categoryId, $foodName, $foodPrice, $imageName);
-    //     return redirect(route('admin.food.index'))->with('success', 'Thêm thực phẩm thành công');
-    // }
-
-    // public function showEditUser(Request $request)
-    // {
-    //     $id = $request->id;
-    //     $allCategory = $this->categoryService->getAll();
-    //     $foodInfo = $this->foodService->getById($id);
-    //     return view('admin.food.edit_food', compact('id', 'foodInfo', 'allCategory'));
-    // }
-
-    // public function editUser(Request $request)
-    // {
-    //     $request->validate([
-    //         'id' => 'required',
-    //         'category_id' => 'required',
-    //         'food_name' => 'required',
-    //         'food_price' => 'required',
-    //     ]);
-    //     $id = $request->id;
-    //     $categoryId = $request->category_id;
-    //     $foodName = $request->food_name;
-    //     $foodPrice = $request->food_price;
-    //     if ($request->food_image) {
-    //         $imageName = time() . '_' . $request->food_image->getClientOriginalName();
-    //         $request->food_image->move(public_path('img'), $imageName);
-    //         $oldImagePath = $this->foodService->getById($request->id)->image;
-    //         if (file_exists(public_path('img') . '/' . $oldImagePath)) {
-    //             unlink(public_path('img') . '/' . $oldImagePath);
-    //         }
-    //     }
-    //     $this->foodService->edit($id, $categoryId, $foodName, $foodPrice, $imageName ?? null);
-    //     return redirect(route('admin.food.index'))->with('success', 'Sửa thực phẩm thành công');
-    // }
-
-    // public function deleteUser(Request $request) {
-    //     $id = $request->id;
-    //     if(!$this->foodService->checkHasChildren($id)) {
-    //         $this->foodService->delete($id);
-    //         return redirect(route('admin.food.index'))->with('success', 'Xóa thực phẩm thành công') ;
-    //     }
-    //     return redirect(route('admin.food.index'))->with('error', 'Thực phẩm đang nằm trong món ăn, không thể xóa !!!');
-    // }
+    public function show($id){
+        $user = $this->userService->getById($id);
+        return view('admin.user.edit', compact('user'));
+    }
+    public function edit($id, Request $request){
+        if ($request->input('action') === 'editBalance') {
+        $this->userService->editbalance($id, $request);
+        // dd($request->all());
+        return redirect()->route('admin.user.index')->with('success', 'Balance updated successfully');
+        }else if($request->input('action') === 'editRole'){
+            $this->userService->editRole($id, $request);
+            return redirect()->route('admin.user.index')->with('success', 'Role updated successfully');
+        }
+    }
 }
