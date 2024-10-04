958a2f1c8b3f5b24c994bf4e8c6baadaff2d4a0a
huylee47 <huyw300@gmail.com>
2024-10-04 16:17:37 +0700

    fix user service

--- backend/app/Service/admin/UserService.php
+++ backend/app/Service/admin/UserService.php
@@ -17,36 +17,75 @@ class UserService
     {
         return User::find($id);
     }
+
     public function editRole($id, Request $request)
     {
         $user = User::where('id', $id)->first();
+        if($request->input('role')== ""){
+            $user->role = User::find($id)->value('role');
+            $user->save();
+            return $user;
+        }
         $user->role = $request->input('role');
         $user->save();
         return $user;
     }
+
     public function editBalance($id, Request $request)
     {
         $user = User::where('id', $id)->first();
         if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
+        return redirect()->route('admin.user.index')->with('message', 'User not found');
         }
-        $balance = $request->input('balance');
-        $plusbalance = $request->input('plusbalance');
-        if (isset($balance) && isset($plusbalance)) {
-            $user->balance =  $balance + $plusbalance;
+        $balance = $user->balance;
+        $addedBalance = $request->input('addedBalance');
+        if (isset($balance) && isset($addedBalance)) {
+            $user->balance =  $balance + $addedBalance;
             $user->save();
-            return response()->json(['message' => 'Balance updated successfully'], 200);
+            return $user;
         }
-        return response()->json(['message' => 'Invalid data provided'], 400);
+        return redirect()->route('admin.user.edit')->with('message', 'Invalid data provided');
+
     }
+
+    public function changePass($id, Request $request)
+    {
+        $user = User::where('id', $id)->first();
+        $request->validate([
+            'password'=> 'required',
+        ],
+        [
+            'password.required' => 'required',
+        ]
+        );
+        if (!$user) {
+        return redirect()->route('admin.user.index')->with('message', 'User not found');
+        }
+        if($request->input('password')==null) {
+            $user->password = User::find($id)->value('password');
+        }
+        $user->password = bcrypt($request->input('password'));
+        $user->save();
+        return $user;
+    }
+
     public function addAccount(Request $request)
     {
-        $check = User::where('username', $request->input('username'))->first();
-
-        if ($check) {
-            return response()->json(['message' => 'Username already exists'], 400);
-        }
-
+        $request->validate([
+            'username' => 'required|string|unique:users,username',
+            'password'=> 'required|string',
+            'email'=> 'required|string|unique:users,email',
+            'phone'=> 'required|string',
+            'role'=> 'required|integer',
+        ],
+        [
+            'username.required' => 'required',
+            'username.unique' => 'username already exists',
+            'email.unique' => 'Email already exists',
+            'phone.required' => 'required',
+            'role.required' => 'required',
+        ]
+        );
         $user = new User();
         $user->username = $request->input('username');
         $user->name = $request->input('name');
@@ -56,41 +95,7 @@ class UserService
         $user->role = $request->input('role');
         $user->balance = $request->input('balance', 0);
         $user->save();
-
-        return response()->json(['message' => 'Account created successfully'], 200);
+        return $user;
     }
-    public function blockUser($id)
-    {
-        $user = User::where('id', $id)->first();
-        if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
-        }
-        $user->role = 3;
-        $user->save();
-        return response()->json(['message' => 'User blocked successfully'], 200);
-    }
-    public function unblockUser($id)
-    {
-        $user = User::where('id', $id)->first();
-        if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
-        }
-        $user->role = 2;
-        $user->save();
-        return response()->json(['message' => 'User unblocked successfully'], 200);
-    }
-    public function changePass($id, Request $request)
-    {
-
-        $user = User::where('id', $id)->first();
-        if (!$user) {
-            return response()->json(['message' => 'User not found'], 404);
-        }
-        $user->password = bcrypt($request->input('password'));
-        $user->save();
-        return response()->json([
-            'message' => 'Password updated successfully',
-            'password' => $user->password,
-        ], 200);
-    }
+    
 }
