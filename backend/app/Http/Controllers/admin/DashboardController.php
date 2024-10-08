e69f968252b40b6fff9bfc1e2267b0d5da5d235e
huylee47 <huyw300@gmail.com>
2024-10-08 09:50:03 +0700

    Adjust dashboard ctrl

--- backend/app/Http/Controllers/admin/DashboardController.php
+++ backend/app/Http/Controllers/admin/DashboardController.php
@@ -17,6 +17,6 @@ class DashboardController extends Controller
     }
     public function index()
     {
-        return view('admin.dashboard.home')->with('data', $this->dashboardService->getDashboard());
+        return view('admin.dashboard.home');
     }
 }
