0fff455351202d1d86e300d5cdba5658c9813e33
huylee47 <huyw300@gmail.com>
2024-10-14 17:30:29 +0700

    update if revenue = 0

--- backend/public/js/admin/chart-pie.js
+++ backend/public/js/admin/chart-pie.js
@@ -1,15 +1,22 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontFamily = 'Nunito, -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
 // Pie Chart Example
 var ctx = document.getElementById("myPieChart");
+
+// Calculate total
+var total = accountBillWeek + percentRerollWeek + percentRechargeWeek;
+
+// If total is 0, set data to show an empty circle
+var data = total === 0 ? [1, 1, 1] : [accountBillWeek, percentRerollWeek, percentRechargeWeek];
+
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
     labels: ["Mua tài Khoản", "Mua acc reroll", "Nạp trong game"],
     datasets: [{
-      data: [accountBillWeek, percentRerollWeek, percentRechargeWeek], 
+      data: data, 
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
@@ -33,7 +40,6 @@ var myPieChart = new Chart(ctx, {
           return label + ': ' + currentValue + '%'; 
         }
       }
-      
     },
     legend: {
       display: false
