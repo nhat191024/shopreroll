b9e40fa35d1fdab52975eb0412d58a06ddba15db
nhat191024 <richberchannel01@gmail.com>
2024-10-15 19:51:21 +0700

    Refactor: Update delete route in web.php to include game status in endpoint

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -24,7 +24,7 @@ Route::prefix('admin')->group(function () {
         Route::post('/add', [GameController::class, 'addGame'])->name('admin.game.add');
         Route::post('/edit', [GameController::class, 'editGame'])->name('admin.game.edit');
         Route::get('/edit/{id}', [GameController::class, 'showEditGame'])->name('admin.game.show_edit');
-        Route::get('/ChangeStatus/{id}', [GameController::class, 'ChangeGameStatus'])->name('admin.game.ChangeStatus');
+        Route::get('/ChangeStatus/{id}/{status}', [GameController::class, 'ChangeGameStatus'])->name('admin.game.ChangeStatus');
     });
 
     Route::prefix('/game-category')->group(function () {
