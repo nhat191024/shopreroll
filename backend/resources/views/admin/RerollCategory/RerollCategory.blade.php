ebc3defba86ce905ea3f4868361c1dc881cb7189
nhat191024 <richberchannel01@gmail.com>
2024-10-15 15:31:00 +0700

    Reformat

--- backend/resources/views/admin/RerollCategory/RerollCategory.blade.php
+++ backend/resources/views/admin/RerollCategory/RerollCategory.blade.php
@@ -64,10 +64,12 @@
                                                     onclick="event.preventDefault(); if (confirm('Bạn chắc chắn muốn ẩn item {{ $item->name }} chứ?')) { window.location.href = '{{ route('admin.RerollCategory.ChangeStatus', $item->id) }}'; }">
                                                     Ẩn </a>
                                             @endif
-                                            <a class="btn btn-info" href="{{ route('admin.RerollCategory.ShowEdit', $item->id) }}">Sửa</a>
                                             <a class="btn btn-info"
-                                            href="{{ route('admin.RerollCategory.Detail', ['id' => $item->id]) }}">Chi
-                                            tiết</a>
+                                                href="{{ route('admin.RerollCategory.ShowEdit', $item->id) }}">Sửa</a>
+                                            <a class="btn btn-info"
+                                                href="{{ route('admin.RerollCategory.Detail', ['id' => $item->id]) }}">
+                                                Chi tiết
+                                            </a>
                                         </td>
                                     </tr>
                                 @endforeach
