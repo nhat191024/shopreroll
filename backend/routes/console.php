3361ddcbb3dbbbdb43ec8890ee9d484660fbd586
huylee47 <huyw300@gmail.com>
2024-11-07 09:10:01 +0700

    Create : Schedule auto update every day

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -8,23 +8,4 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
-Artisan::command('game:update-data', function (ApiService $apiService) {
-    try {
-        // CALL FUNCTION TO GET DATA FROM SERVICE
-        $apiService->getGenshinImpactCharacters();
-        $apiService->getHonkaiStarRailCharacters();
-        $apiService->getZenlessZoneZeroCharacters();
-        $apiService->getGenshinImpactWeapons();
-        $apiService->getHonkaiStarRailWeapons();
-        $apiService->getZenlessZoneZeroWeapons();
-
-        $this->info('Game data updated successfully!');
-    } catch (\Exception $e) {
-        $this->error('Error updating game data: ' . $e->getMessage());
-    }
-})->purpose('Update game character and weapon data');
-
-// UPDATE PER 3 DAYS
-Artisan::command('schedule:run', function () {
-    $this->call('game:update-data');
-})->everyThreeDays()->purpose('Schedule the game data update');
+Schedule::command('app:run-update-data-job')->everyMinute();
