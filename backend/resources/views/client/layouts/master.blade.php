cf6affc9cd11bc2a2852d0d16a9cfc038d2f9e97
nhat191024 <richberchannel01@gmail.com>
2024-12-18 05:40:39 +0700

    refactor: reformat code, comment some unused cdn link

--- backend/resources/views/client/layouts/master.blade.php
+++ backend/resources/views/client/layouts/master.blade.php
@@ -2,43 +2,30 @@
 <html lang="vi">
 
 <head>
+    <title>Shop bán acc Honkai Star Rail và Genshin uy tín hàng đầu Việt Nam</title>
+
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Shop bán acc Honkai Star Rail và Genshin uy tín hàng đầu Việt Nam</title>
-    <meta name="description" content="Mua bán tài khoản Honkai Star Rail, Shop acc Honkai Star Rail VIP, Reroll uy tín hàng đầu Việt Nam">
-    <meta name="keywords" content="Shop acc Honkai Star Rail VIP,Mua bán tài khoản Honkai Star Rail,Mua Acc Genshin Giá Rẻ Nhất,reroll" />
+    <meta name="description"
+        content="Mua bán tài khoản Honkai Star Rail, Shop acc Honkai Star Rail VIP, Reroll uy tín hàng đầu Việt Nam">
+    <meta name="keywords"
+        content="Shop acc Honkai Star Rail VIP,Mua bán tài khoản Honkai Star Rail,Mua Acc Genshin Giá Rẻ Nhất,reroll" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta property='og:image' content='https://img.upanh.tv/2023/05/17/12312312312-01-01.png' />
+
     <base href="https://shopreroll.com">
-    
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="1ffmFhe89aAR2ncAaRmLV5SaAWF3cUlSiHQmJ3GC">
-    
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    
-    <!-- Bootstrap 4 -->
+    <link rel="icon" type="image/png" href="https://img.upanh.tv/2023/05/17/image84b9fdeeb04998fd.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-    
-    <!-- AdminLTE -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1.0/dist/css/adminlte.min.css">
-    
-    <!-- DataTables -->
-    <link href="{{ url('') . '/' }}vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-    
-    <!-- SweetAlert2 -->
-    <link rel="stylesheet" href="./Acc đã mua_files/sweetalert2.min.css" />
-    
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="{{ url('') . '/' }}css/nav-foot.css">
-    <link rel="stylesheet" href="{{ url('') . '/' }}css/my-acc.css">
-    
-    <!-- Favicon -->
-    <link rel="icon" type="image/png" href="https://img.upanh.tv/2023/05/17/image84b9fdeeb04998fd.png" />
-    
-    <!-- Open Graph Image -->
-    <meta property="og:image" content="https://img.upanh.tv/2023/05/17/12312312312-01-01.png" />
-    
-    <link rel="canonical" href="" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <!-- Theme style -->
+
+    <link rel="canonical" href="https://shopreroll.com" />
+
+    <script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js"
+        data-cf-settings="58c421c51abc89ff9fecda8d-|49"></script>
 </head>
 
 <body class="hold-transition sidebar-collapse layout-top-nav">
@@ -46,50 +33,99 @@
         <!-- Navbar -->
         <nav class="main-header navbar navbar-expand-md nav-header mb-4">
             <div class="container">
-                <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"><i class="fas fa-bars" style="text-shadow: 2px 2px 2px #000000;color: #fff;"></i></span>
+
+                <button class="navbar-toggler order-1" type="button" data-toggle="collapse"
+                    data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon">
+                        <i class="fas fa-bars" style="text-shadow: 2px 2px 2px #000000;color: #fff;"></i>
+                    </span>
                 </button>
 
                 <div class="collapse navbar-collapse order-3" id="navbarCollapse">
                     <!-- Left navbar links -->
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a href="/" class="nav-link"><img src="https://img.upanh.tv/2023/05/17/12312312312-01-01.png" class="img-fluid" style="margin-top: -8px;height: 165%"></a>
+                            <a href="/" class="nav-link">
+                                <img src="https://img.upanh.tv/2023/05/17/12312312312-01-01.png" class="img-fluid"
+                                    style="margin-top: -8px;height: 165%">
+                            </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link menu-header shine-active"><i class="ficon fa-lg fa fa-home"></i></a>
+                            <a href="#" class="nav-link menu-header shine-active">
+                                <i class="ficon fa-lg fa fa-home"></i>
+                            </a>
                         </li>
                         <li class="nav-item dropdown" id="topUp_balance">
-                            <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle menu-header ">Nạp tiền</a>
+                            <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+                                class="nav-link dropdown-toggle menu-header ">
+                                Nạp tiền
+                            </a>
                             <ul class="dropdown-menu border-0 shadow" style="left: 0px; right: inherit;">
-                                <li><a href="https://shopreroll.com/user/money/phone-card/send-card" class="dropdown-item "><i class="fas fa-money-check-alt mr-1"></i> Nạp bằng thẻ cào</a></li>
+                                <li>
+                                    <a href="https://shopreroll.com/user/money/phone-card/send-card"
+                                        class="dropdown-item "><i class="fas fa-money-check-alt mr-1"></i>
+                                        Nạp bằng thẻ cào
+                                    </a>
+                                </li>
                                 <li class="dropdown-divider"></li>
-                                <li><a href="https://shopreroll.com/user/money/auto-bank/info" class="dropdown-item"><i class="fas fa-university mr-1"></i> Nạp bằng bank, ví</a></li>
+                                <li>
+                                    <a href="https://shopreroll.com/user/money/auto-bank/info" class="dropdown-item">
+                                        <i class="fas fa-university mr-1"></i>
+                                        Nạp bằng bank, ví
+                                    </a>
+                                </li>
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link menu-header">Nạp Game</a>
+                            <a href="#" class="nav-link menu-header">
+                                Nạp Game
+                            </a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle menu-header">Lịch Sử Mua</a>
+                            <a href="#" data-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false"class="nav-link dropdown-toggle menu-header">
+                                Lịch Sử Mua
+                            </a>
                             <ul class="dropdown-menu border-0 shadow" style="left: 0px; right: inherit;">
-                                <li><a href="https://shopreroll.com/genshin/acc/my-acc" class="dropdown-item "><i class="fas fa-history mr-1"></i> Genshin</a></li>
+                                <li>
+                                    <a href="https://shopreroll.com/genshin/acc/my-acc" class="dropdown-item ">
+                                        <i class="fas fa-history mr-1"></i>
+                                        Genshin
+                                    </a>
+                                </li>
                                 <li class="dropdown-divider"></li>
-                                <li><a href="https://shopreroll.com/key/my-key" class="dropdown-item "><i class="fas fa-history mr-1"></i> Key / Reroll</a></li>
+                                <li>
+                                    <a href="https://shopreroll.com/key/my-key" class="dropdown-item ">
+                                        <i class="fas fa-history mr-1"></i>
+                                        Key / Reroll
+                                    </a>
+                                </li>
                                 <li class="dropdown-divider"></li>
-                                <li><a href="https://shopreroll.com/acc/my-acc" class="dropdown-item"><i class="fas fa-history mr-1"></i> Acc chung</a></li>
+                                <li>
+                                    <a href="https://shopreroll.com/acc/my-acc" class="dropdown-item">
+                                        <i class="fas fa-history mr-1"></i>
+                                        Acc chung
+                                    </a>
+                                </li>
                             </ul>
                         </li>
                     </ul>
+
                 </div>
 
                 <!-- Right navbar links -->
-                <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto" style="position: absolute;right: 0px;">
+                <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto"
+                    style="position: absolute;right: 0px;">
                     <li class="nav-item">
-                        <a href="#"><button class="btn-pretty">Đăng ký</button></a>
+                        <a href="#">
+                            <button class="btn-pretty">Đăng ký</button>
+                        </a>
                     </li>
                     <li class="nav-item">
-                        <a href="#"><button class="btn-pretty">Đăng nhập</button></a>
+                        <a href="#">
+                            <button class="btn-pretty">Đăng nhập</button>
+                        </a>
                     </li>
                 </ul>
             </div>
@@ -98,78 +134,97 @@
 
         @yield('main')
 
-        <!-- Footer -->
-        <footer class="mt-5 p-3">
-            <div class="container-lg">
-                <h2 class="guide__title mt-3">
-                    <a href="/"><img src="https://img.upanh.tv/2023/05/17/12312312312-01-01.png" style="margin-top: -8px;height: 45px"></a>
-                </h2>
-                <div class="row">
-                    <section class="col-12 col-lg-4">
-                        <div class="h h4 link-active">Về <a href="/" style="color: inherit;text-shadow:unset">shopreroll.com</a></div>
-                        <p><span class="text-white font-system-ui"><b>Chúng tôi làm việc một cách chuyên nghiệp, uy tín, nhanh chóng và luôn đặt quyền lợi của bạn lên hàng đầu</b></span><br></p>
-                        <p class="mt-3 small">© shopreroll.com</p>
-                        <div class="h h5"><i class="fa fa-language"></i> Ngôn ngữ</div>
-                    </section>
+    </div>
+    <footer class="mt-5 p-3">
+        <div class="container-lg">
+            <h2 class="guide__title mt-3">
+                <a href="/">
+                    <img src="https://img.upanh.tv/2023/05/17/12312312312-01-01.png"
+                        style="margin-top: -8px;height: 45px">
+                </a>
+            </h2>
+            <div class="row">
+                <section class="col-12 col-lg-4">
+                    <div class="h h4 link-active">
+                        Về <a href='/' style="color: inherit;text-shadow:unset">shopreroll.com</a>
+                    </div>
+                    <p>
+                        <span class="text-white font-system-ui">
+                            <b>
+                                Chúng tôi làm việc một cách chuyên nghiệp, uy tín, nhanh chóng và luôn đặt quyền lợi của
+                                bạn lên hàng đầu
+                            </b>
+                        </span>
+                        <br>
+                    </p>
+                    <p class="mt-3 small">
+                        © shopreroll.com
+                    </p>
+                    <div class="h h5">
+                        <i class="fa fa-language"></i> Ngôn ngữ
+                    </div>
+                </section>
 
-                    <section class="col-12 col-lg-4">
-                        <div class="h h4 link-active">Chúng tôi</div>
-                        <p><span class="text-white font-system-ui"><b>Tất Cả Khách Hàng mua acc trên Shop Đều được Bảo hành 100%.<br>Khách hàng mua acc trên shop phải chủ động đổi |mật khẩu| và |mail| ngay trong vòng 24h ngoài 24h shop ko chịu trách nhiệm.</b></span><br></p>
-                    </section>
-
-                    <section class="col-12 col-lg-1"></section>
-
-                    <section class="col-12 col-lg-3">
-                        <i class="fab fa-facebook-square fa-2x mr-2"></i>
-                        <i class="fab fa-youtube fa-2x"></i>
-                        <p class="mt-3 fw-bold"><i class="fa fa-phone mr-2"></i>Hotline: 0386496488</p>
-                        <p class="fw-bold"><i class="fa fa-clock mr-2"></i>Work time: 12h - 24h</p>
-                        <p class="fw-bold"><i class="fa fa-map-marked-alt mr-2"></i>Address: Ba Đình-HN</p>
-                    </section>
-                </div>
+                <section class="col-12 col-lg-4">
+                    <div class="h h4 link-active">Chúng tôi</div>
+                    <p>
+                        <span class="text-white font-system-ui">
+                            <b>Tất Cả Khách Hàng mua acc trên Shop Đều được Bảo hành 100%.
+                                <br>
+                                Khách hàng mua acc trên shop phải chủ động đổi |mật khẩu| và |mail| ngay trong vòng 24h
+                                ngoài 24h shop ko chịu trách nhiệm.
+                            </b>
+                        </span>
+                        <br>
+                    </p>
+                </section>
+                <section class="col-12 col-lg-1">
+                </section>
+                <section class="col-12 col-lg-3">
+                    <i class="fab fa-facebook-square fa-2x mr-2"></i>
+                    <i class="fab fa-youtube fa-2x"></i>
+                    <p class="mt-3 fw-bold"><i class="fa fa-phone mr-2"></i>Hotline: 0386496488</p>
+                    <p class="fw-bold"><i class="fa fa-clock mr-2"></i>Work time: 12h - 24h</p>
+                    <p class="fw-bold"><i class="fa fa-map-marked-alt mr-2"></i>Address: Ba Đình-HN</p>
+                    <p class="m-0"></p>
+                </section>
             </div>
-        </footer>
+        </div>
+    </footer>
     </div>
 
-    <!-- Fixed Navigation -->
+    </section>
+
+    </div>
+    <link rel="stylesheet" href="{{ url('') . '/' }}css/nav-foot.css">
+    <link rel="stylesheet" href="{{ url('') . '/' }}css/home.css">
     <ul class="nav-fixed">
         <li class="nav-fixed-zalo">
-            <a target="_blank" href="https://zalo.me/0386496488" title="Chat với chúng tôi trên Zalo">
-                <i class="fab fa-zalo"></i>
+            <a target="_blank" href="https://zalo.me/0386496488"><img src="style/images/icon/zalo.png"></a>
+        </li>
+        <li class="nav-fixed-face">
+            <a target="_blank" href="https://www.facebook.com/dat.ds.3">
+                <i style="color: white"class="fab fa-facebook-f fa-lg"></i>
             </a>
         </li>
-        <li class="nav-fixed-yt">
-            <a href="https://www.youtube.com" target="_blank">
-                <i class="fab fa-youtube"></i>
+        <li class="nav-fixed-phone">
+            <a href="tel:0386496488">
+                <i style="color: white" class="fa fa-phone fa-lg"></i>
             </a>
         </li>
     </ul>
 
-    <!-- Scripts -->
-<!-- Tải jQuery từ CDN (đảm bảo là đường dẫn đúng và tải đầu tiên) -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    var $jq = jQuery.noConflict();  
-    $jq(document).ready(function() {
-        // Bạn có thể sử dụng $jq thay vì $ ở đây
-        $jq('#dataTable').DataTable();  // Thay "example" bằng ID của bảng DataTable bạn đang sử dụng
-    });
-</script>
+    <!-- jQuery -->
+    {{-- <script src="style/plugins/jquery/jquery.min.js" type="58c421c51abc89ff9fecda8d-text/javascript"></script> --}}
+    <!-- Bootstrap 4 -->
+    {{-- <script src="style/plugins/bootstrap/js/bootstrap.bundle.min.js" type="58c421c51abc89ff9fecda8d-text/javascript"></script> --}}
+    {{-- <script src="style/dist/js/adminlte.min.js" type="58c421c51abc89ff9fecda8d-text/javascript"></script> --}}
+    {{-- <script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="58c421c51abc89ff9fecda8d-|49" defer></script> --}}
 
-<!-- Tải DataTables (phải sau jQuery) -->
-<script src="{{ url('') . '/' }}vendor/datatables/jquery.dataTables.min.js"></script>
-<script src="{{ url('') . '/' }}vendor/datatables/dataTables.bootstrap4.min.js"></script>
-
-<!-- Tải Bootstrap từ CDN -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-
-<!-- Tải AdminLTE từ CDN -->
-<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1.0/dist/js/adminlte.min.js"></script>
-
-<!-- Các script khác như Cloudflare Rocket Loader có thể giữ lại -->
-<script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="58c421c51abc89ff9fecda8d-|49" defer></script>
-
-
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1.0/dist/js/adminlte.min.js"></script>
 </body>
 
 </html>
