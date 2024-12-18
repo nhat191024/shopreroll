9a1750597497c4db69f4640df43986d4c524d7f3
huylee47 <huyw300@gmail.com>
2024-12-18 13:19:11 +0700

    Add: route sync

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -38,6 +38,8 @@ Route::prefix('admin')->group(function () {
     });
     Route::get('characters/{slug}', [ApiController::class, 'indexGameCharacters'])->name('admin.character');
     Route::get('weapons/{slug}', [ApiController::class, 'indexGameWeapons'])->name('admin.weapon');
+    Route::get('/syncCharacters', [ApiController::class, 'syncHeroData'])->name('syncCharacters');
+    Route::get('/syncWeapons', [ApiController::class, 'syncWeaponData'])->name('syncWeapons');
 
     Route::prefix('/game-category')->group(function () {
         Route::get('/list/{id}', [GameCategoryController::class, 'index'])->name('admin.GameCategory.index');
