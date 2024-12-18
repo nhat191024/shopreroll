66f3c80121c2b9302df7561c7e6641245d4dcf6e
huylee47 <huyw300@gmail.com>
2024-12-18 10:03:49 +0700

    update : login view and logic

--- backend/app/Service/LoginService.php
+++ backend/app/Service/LoginService.php
@@ -9,19 +9,20 @@ use Illuminate\Support\Facades\Hash;
 
 class LoginService {
     public function index(){
-    return view('/');
+    return view('client.layouts.login');
     }
     public function login(Request $request) {
-        $email = $request->input('email');
+        $username = $request->input(key: 'username');
         $password = $request->input('password');
-        $account = User::where('email', $email)->first();
+        $account = User::where('username', $username)->first();
         if (!$account) {
             return redirect('/login')->with('error', 'Không tìm thấy tài khoản');
         }
         if (!Hash::check($password, $account->password)) {
             return redirect('/login')->with('error', 'Thông tin đăng nhập không chính xác');
         }
-        return redirect('/')->with('message', 'Đăng nhập thành công');
+        // return redirect('/')->with('message', 'Đăng nhập thành công');
+        return ('đăng nhập thành công');
     }
     public function logout() {
         Auth::logout();
