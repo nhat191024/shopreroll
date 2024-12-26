652dda798c3f858c780565357c0de2693c036556
Lee <letnp1024@gmail.com>
2024-12-26 20:33:49 +0700

    Thêm các files (dùng file master2 vì thêm nhiều thư viện mới)

--- backend/resources/views/client/myKey/index.blade.php
+++ backend/resources/views/client/myKey/index.blade.php
@@ -1,42 +1,81 @@
-@extends('client.layouts.master')
+@extends('client.layouts.master2')
 @section('main')
 
-    <section class="content">
-            <div class="container-fluid">
-                <center><img src="./Key bought_files/2020031919242255224.png" class="city__icon"></center>
-                <h1 class="guide__title">Key bought </h1>
-                <main>
-                <div>
-                            <!-- Default box -->
-    <div class="card card-solid offset-lg-1 col-lg-10">
-        <div class="card-body">
+<section class="content">
+    <div class="container-fluid">
+        <center>
+            <img src="https://uploadstatic-sea.mihoyo.com/contentweb/20200319/2020031919242255224.png" class="city__icon">
+        </center>
+        <h1 class="guide__title">Key bought</h1>
+        <main>
+            <div>
+                <!-- Default box -->
+                <div class="card card-solid offset-lg-1 col-lg-10">
+                    <div class="card-body">
                         <div class="col-lg-12">
-                <div class="mt-3 table-responsive">
-                                            <a href="https://shopreroll.com/key/my-key?raw=view" class="btn btn-sm btn-primary">Xem giản lược</a>
-                                        <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4"><div class="row"><div class="col-sm-12 col-md-6"><div class="dt-buttons btn-group flex-wrap">          <button class="btn btn-secondary buttons-copy buttons-html5" tabindex="0" aria-controls="example1" type="button"><span>Copy</span></button> <button class="btn btn-secondary buttons-csv buttons-html5" tabindex="0" aria-controls="example1" type="button"><span>Excel</span></button> <button class="btn btn-secondary buttons-print" tabindex="0" aria-controls="example1" type="button"><span>Print</span></button> <div class="btn-group"><button class="btn btn-secondary buttons-collection dropdown-toggle buttons-colvis" tabindex="0" aria-controls="example1" type="button" aria-haspopup="true" aria-expanded="false"><span>Column visibility</span></button></div> </div></div><div class="col-sm-12 col-md-6"><div id="example1_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example1"></label></div></div></div><div class="row"><div class="col-sm-12"><table id="example1" class="table table-bordered table-striped dataTable dtr-inline" data-order="[[3, &quot;desc&quot;]]" data-page-length="20" role="grid" aria-describedby="example1_info">
-                        <thead>
-                        <tr role="row"><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="ID: activate to sort column ascending">ID</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Key: activate to sort column ascending">Key</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Thông tin
-: activate to sort column ascending">Thông tin
-</th><th class="sorting sorting_desc" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="descending" aria-label="Mua lúc
-: activate to sort column ascending" style="">Mua lúc
-</th></tr>
-                        </thead>
-                        <tbody>
-                                                                                                    <tr class="odd"><td valign="top" colspan="4" class="dataTables_empty">No data available in table</td></tr></tbody>
-                        <tfoot>
-                        <tr><th rowspan="1" colspan="1">ID</th><th rowspan="1" colspan="1">Key</th><th rowspan="1" colspan="1">Thông tin
-</th><th rowspan="1" colspan="1" style="">Mua lúc
-</th></tr>
-                        </tfoot>
-                    </table></div></div><div class="row"><div class="col-sm-12 col-md-5"><div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 0 to 0 of 0 entries</div></div><div class="col-sm-12 col-md-7"><div class="dataTables_paginate paging_simple_numbers" id="example1_paginate"><ul class="pagination"><li class="paginate_button page-item previous disabled" id="example1_previous"><a href="https://shopreroll.com/#" aria-controls="example1" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li><li class="paginate_button page-item next disabled" id="example1_next"><a href="https://shopreroll.com/#" aria-controls="example1" data-dt-idx="1" tabindex="0" class="page-link">Next</a></li></ul></div></div></div></div>
+                            <div class="mt-3 table-responsive">
+                                <button id="raw-button" class="btn btn-sm btn-primary mb-3">Xem giản lược</button>
+                                <input type="hidden" id="raw-content" value="@foreach ($keys as $item)- {{ $item->key }}&#10;@endforeach">
+                                <textarea id="raw-textarea" rows="10" class="form-control" style="display: none; text-align: left;" readonly></textarea>
+                                <table id="user_data" class="table table-striped table-bordered" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Key</th>
+                                            <th>Thông tin</th>
+                                            <th>Mua lúc</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($keys as $key => $item)
+                                            <tr>
+                                                <td>{{ $item->id }}</td>
+                                                <td>{{ $item->key }}</td>
+                                                <td><b>{{ $item->rerollPackage }}</b></br>Giá: {{ number_format($item->price, 0, ',') }} VNĐ VNĐ</td>
+                                                <td>{{ $item->purchased_at }}</td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
                 </div>
+                <!-- /.card -->
             </div>
-        </div>
-        <!-- /.card-body -->
+        </main>
     </div>
-    <!-- /.card -->
+    <!-- Custom JavaScript -->
+    <script>
+      $(document).ready(function () {
+        var dataTable1 = $('#user_data').DataTable({
+          dom: 'Bfrtip',
+          buttons: ['copy', 'excel', 'print', 'colvis']
+        });
+      });
+    document.addEventListener('DOMContentLoaded', function () {
+      const rawButton = document.getElementById('raw-button');
+      const rawTextarea = document.getElementById('raw-textarea');
+  
+      rawButton.addEventListener('click', function () {
+        // Toggle visibility of the textarea
+        if (rawTextarea.style.display === 'none' || rawTextarea.style.display === '') {
+          rawTextarea.style.display = 'block'; // Show textarea
+          rawButton.textContent = 'Thoát giản lược'; // Change button text
+        } else {
+          rawTextarea.style.display = 'none'; // Hide textarea
+          rawButton.textContent = 'Xem giản lược'; // Revert button text
+        }
+      });
+    });
+    // Populate the textarea with the hidden input's value
+    const content = document.getElementById('raw-content').value;
+    document.getElementById('raw-textarea').value = content;
+</script>
+  </body>
+</html>
 
-                </div>
-                </main>
-                
+
 @endsection
