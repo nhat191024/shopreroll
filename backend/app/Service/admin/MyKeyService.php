9c4a205a8b0eb4ac090311970b3654b10120e31c
nhat191024 <richberchannel01@gmail.com>
2025-02-23 18:23:13 +0700

    Refactor MyKeyService to improve code readability and maintainability

--- backend/app/Service/admin/MyKeyService.php
+++ backend/app/Service/admin/MyKeyService.php
@@ -1,26 +1,30 @@
 <?php
 
 namespace App\Service\admin;
+
 use Illuminate\Support\Facades\Auth;
 use App\Models\RerollKey;
 use App\Models\RerollBill;
 use App\Service\admin\RerollPackageService;
+
 class MyKeyService
 {
     private $rerollPackageService;
+    
     public function __construct(RerollPackageService $rerollPackageService)
     {
         $this->rerollPackageService = $rerollPackageService;
     }
+
     public function getAuthKeys()
     {
         $userId = Auth::id();
         // Fetch the key IDs and their `created_at` timestamps
         $keyDetails = RerollBill::where('user_id', $userId)->select('reroll_key_id', 'created_at', 'price')->get();
-    
+
         // Fetch all reroll keys in one query
         $rerollKeys = RerollKey::whereIn('id', $keyDetails->pluck('reroll_key_id'))->get();
-    
+
         // Map `created_at` from the keyDetails to the corresponding reroll key
         $rerollKeys = $rerollKeys->map(function ($key) use ($keyDetails) {
             $keyDetail = $keyDetails->firstWhere('reroll_key_id', $key->id);
@@ -31,7 +35,4 @@ class MyKeyService
         });
         return $rerollKeys;
     }
-    
-
-    
 }
