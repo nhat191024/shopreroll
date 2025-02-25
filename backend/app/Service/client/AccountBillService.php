07eeea483fcca28264891802ef40ef94caa62452
Catty <ducvu73@gmail.com>
2025-02-25 18:02:44 +0700

    update: fix fetching and only filter out genshin related bills

--- backend/app/Service/client/AccountBillService.php
+++ backend/app/Service/client/AccountBillService.php
@@ -3,20 +3,27 @@
 namespace App\Service\client;
 
 use App\Models\AccountBill;
+use App\Models\Game;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
-class AccountBillService {
-    public function getAllByUserId($id)
+class AccountBillService
+{
+    public function getAllGenshinBillByUserId($id)
     {
+        $genshinGameId = Game::where('name', 'LIKE', '%Genshin%')->first()->id;
         if (is_null($id)) {
             // prevent null
             return collect();
         }
-        $accountBill = AccountBill::where('user_id', $id)->get();
+        $accountBill = AccountBill::where('user_id', $id)
+            ->whereHas('GameAccount', function ($query) use ($genshinGameId) {
+                $query->whereHas('GameCategory', function ($query) use ($genshinGameId) {
+                    $query->where('game_id', $genshinGameId);
+                });
+            })->get();
         return $accountBill;
     }
 }
-?>
