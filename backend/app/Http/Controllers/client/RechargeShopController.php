09df86b78a332319bb46a52d1718870673eedeb6
Catty <ducvu73@gmail.com>
2025-02-26 20:35:03 +0700

    Reapply "Merge branch 'Dev' of https://github.com/nhat191024/shopreroll into Dev"
    This reverts commit 71481cdeff8d7e5288a1b34f12c10e9640a0bfc7.

---
+++ backend/app/Http/Controllers/client/RechargeShopController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\client;
+
+use App\Http\Controllers\Controller;
+use App\Service\admin\GameRechargeService;
+use App\Service\admin\RerollCategoryService;
+use App\Service\client\RechargeShopService;
+use Illuminate\Http\Request;
+
+class RechargeShopController extends Controller
+{
+    private $rechargeShopService;
+    public function __construct(RechargeShopService $rechargeShopService)
+    {
+        $this->rechargeShopService = $rechargeShopService;
+    }
+    public function index($id)
+    {
+        if (!auth()->check()) {
+            return redirect()->route('login');
+        }
+        $gameRecharge = $this->rechargeShopService->getCurrentGameRechargeById($id);
+        $rechargePackages = $gameRecharge->rechargePackages;
+        $rechargeBills = $this->rechargeShopService->getAllRechargeBillsByUser($gameRecharge->id);
+        return view('client.home.game-recharge', compact('rechargePackages','gameRecharge', 'rechargeBills'));
+    }
+
+    public function rechargeConfirm(Request $request)
+    {
+        if (!auth()->check()) {
+            return redirect()->route('login');
+        }
+        $this->rechargeShopService->rechargeConfirm($request);
+        return redirect()->route('client.home');
+    }
+}
