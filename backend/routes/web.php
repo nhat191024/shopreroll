cdb852ed1d4d7c473a4acb85c6b5da24739e92d3
Catty <ducvu73@gmail.com>
2025-02-26 20:33:24 +0700

    Update web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -53,6 +53,7 @@ Route::middleware(['auth', 'role:0,1,2'])->group(function () {
 Route::get('/my-account', function () {
     return view('client.my-account');
     // return 1;
+
 });
 
 // Note: route 0=userClient, 1=admin, 2=collaborator
