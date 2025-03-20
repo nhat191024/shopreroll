b75130193b55d7189326f6970826754d2adbfc18
nhat191024 <richberchannel01@gmail.com>
2025-03-21 02:07:33 +0700

    reformat

--- resources/views/admin/game/AddGame.blade.php
+++ resources/views/admin/game/AddGame.blade.php
@@ -4,24 +4,23 @@
 
         <h1 class="h3 mb-2 text-gray-800">Thêm trò chơi</h1>
 
-        <div class="card shadow mb-4">
+        <div class="card mb-4 shadow">
             <div class="card-body">
                 <div class="table-responsive">
-                    <form action="{{ route('admin.game.add') }}" method="post" enctype="multipart/form-data" class="form">
+                    <form class="form" action="{{ route('admin.game.add') }}" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group">
                             <label for="">Tên trò chơi</label>
-                            <input required type="text" class="form-control" id="" aria-describedby=""
-                                name="name">
+                            <input id="" class="form-control" name="name" type="text" required>
                         </div>
                         <div class="game-item">
                             <div class="form-group">
                                 <label for="">Tên game item</label>
-                                <input required type="text" class="form-control" id="" aria-describedby="game-item-name" name="game_item[]">
+                                <input id="" class="form-control" name="game_item[]" type="text" required aria-describedby="game-item-name">
                             </div>
                         </div>
                         <button class="btn btn-success mt-4" type="submit">Thêm trò chơi</button>
-                        <button class="btn btn-success mt-4" id="add-game-item-btn" type="button">Thêm game item</button>
+                        <button id="add-game-item-btn" class="btn btn-success mt-4" type="button">Thêm game item</button>
                     </form>
                 </div>
             </div>
