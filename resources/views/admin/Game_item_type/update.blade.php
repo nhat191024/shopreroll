b52203abd6baa72d8c6741f79c1f754c38b18a42
nhat191024 <richberchannel01@gmail.com>
2025-03-24 09:18:26 +0700

    feat: create update view for GameItemType with form and navigation

---
+++ resources/views/admin/Game_item_type/update.blade.php
@@ -0,0 +1,24 @@
+@extends('admin.master')
+@section('main')
+    <div class="container-fluid">
+
+        <h1 class="h3 mb-2 text-gray-800">Sửa vật phẩm {{ $gameItemType->name }}</h1>
+
+        <div class="card mb-4 shadow">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <form class="form" action="{{ route('admin.game_item_type.update', [$gameItemType->id]) }}" method="post">
+                        @csrf
+                        <div class="form-group mb-3">
+                            <label for="">Tên vật phẩm</label>
+                            <input id="" class="form-control" name="name" type="text" required aria-describedby="game-item-name" value="{{ $gameItemType->name }}">
+                        </div>
+                        <button class="btn btn-success mt-4" type="submit">Xác nhận</button>
+                        <a class="btn btn-primary mt-4" href="{{ url()->previous() }}">Quay lại</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
