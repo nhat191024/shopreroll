75075de7f41a974601cc8255aa1c7196285ef9d0
nhat191024 <richberchannel01@gmail.com>
2025-03-24 09:29:28 +0700

    feat: add error display for form validation in GameItemType update view

--- resources/views/admin/Game_item_type/update.blade.php
+++ resources/views/admin/Game_item_type/update.blade.php
@@ -6,15 +6,25 @@
 
         <div class="card mb-4 shadow">
             <div class="card-body">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        Lỗi xảy ra, vui lòng kiểm tra lại thông tin nhập vào
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
                 <div class="table-responsive">
                     <form class="form" action="{{ route('admin.game_item_type.update', [$gameItemType->id]) }}" method="post">
                         @csrf
                         <div class="form-group mb-3">
                             <label for="">Tên vật phẩm</label>
-                            <input id="" class="form-control" name="name" type="text" required aria-describedby="game-item-name" value="{{ $gameItemType->name }}">
+                            <input id="" class="form-control" name="name" type="text" aria-describedby="game-item-name" value="{{ $gameItemType->name }}">
                         </div>
                         <button class="btn btn-success mt-4" type="submit">Xác nhận</button>
-                        <a class="btn btn-primary mt-4" href="{{ url()->previous() }}">Quay lại</a>
+                        <a class="btn btn-primary mt-4" href="{{ route('admin.game_item_type.index', [$gameItemType->game_id]) }}">Quay lại</a>
                     </form>
                 </div>
             </div>
