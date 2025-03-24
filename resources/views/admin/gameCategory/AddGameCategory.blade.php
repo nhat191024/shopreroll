59230d0e10ff012b9adba51afee18ce86f134b86
nhat191024 <richberchannel01@gmail.com>
2025-03-24 16:28:21 +0700

    Refactor: clean up HTML structure and improve form input attributes in AddGameCategory and EditGameCategory views

--- resources/views/admin/gameCategory/AddGameCategory.blade.php
+++ resources/views/admin/gameCategory/AddGameCategory.blade.php
@@ -1,34 +1,26 @@
 @extends('admin.master')
 @section('main')
-    <!-- Content Wrapper -->
-
-
-    <!-- Begin Page Content -->
     <div class="container-fluid">
-
-        <!-- Page Heading -->
         <h1 class="h3 mb-2 text-gray-800">Thêm danh mục</h1>
-        <!-- DataTales Example -->
-        <div class="card shadow mb-4">
+        <div class="card mb-4 shadow">
             <div class="card-body">
                 <div class="table-responsive">
                     <form action="{{ route('admin.GameCategory.add') }}" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group">
                             <label for="">Tên danh mục</label>
-                            <input required type="text" class="form-control" id="" aria-describedby=""
-                                name="category_name" placeholder="Nhập tên danh mục bằng Tiếng Việt">
+                            <input id="" class="form-control" name="category_name" type="text" required aria-describedby="" placeholder="Nhập tên danh mục bằng Tiếng Việt">
                         </div>
                         <div class="form-group">
                             <label for="category_image">Hình ảnh danh mục</label>
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="category_image" name="category_image">
+                                <input id="category_image" class="custom-file-input" name="category_image" type="file">
                                 <label class="custom-file-label" for="category_image">Chọn ảnh</label>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="product_id">Chọn game</label>
-                            <select class="form-control" id="product_id" name="game_id">
+                            <select id="product_id" class="form-control" name="game_id">
                                 @foreach ($game as $item)
                                     <option value="{{ $item->id }}">
                                         {{ $item->name }}
@@ -44,14 +36,6 @@
         </div>
 
     </div>
-    <!-- /.container-fluid -->
-
-    </div>
-    <!-- End of Main Content -->
-
-
-
-    <!-- End of Content Wrapper -->
     <script>
         // Add the following code if you want the name of the file appear on select
         $(".custom-file-input").on("change", function() {
--- resources/views/admin/gameCategory/EditGameCategory.blade.php
+++ resources/views/admin/gameCategory/EditGameCategory.blade.php
@@ -1,43 +1,35 @@
 @extends('admin.master')
 @section('main')
-    <!-- Content Wrapper -->
-
-
-    <!-- Begin Page Content -->
     <div class="container-fluid">
-
-        <!-- Page Heading -->
         <h1 class="h3 mb-2 text-gray-800">Sửa danh mục</h1>
-        <!-- DataTales Example -->
-        <div class="card shadow mb-4">
+        <div class="card mb-4 shadow">
             <div class="card-body">
                 <div class="table-responsive">
                     <form action="{{ route('admin.GameCategory.edit') }}" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group">
                             <label for="">Tên danh mục</label>
-                            <input required type="text" class="form-control" id="" aria-describedby=""
-                                name="category_name" placeholder="Nhập tên danh mục bằng Tiếng Việt" value="{{ $gameCategoryInfo['name'] }}">
+                            <input id="" class="form-control" name="category_name" type="text" required value="{{ $gameCategoryInfo['name'] }}" aria-describedby="" placeholder="Nhập tên danh mục bằng Tiếng Việt">
                         </div>
                         <div class="form-group">
                             <label for="category_image">Hình ảnh danh mục (Bỏ trống nếu không muốn cập nhật ảnh)</label>
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="category_image" name="category_image">
+                                <input id="category_image" class="custom-file-input" name="category_image" type="file">
                                 <label class="custom-file-label" for="category_image">Chọn ảnh</label>
 
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="game_id">Chọn game</label>
-                            <select required class="form-control" id="game_id" name="game_id">
+                            <select id="game_id" class="form-control" name="game_id" required>
                                 @foreach ($game as $item)
-                                    <option value="{{ $item->id }}" {{ $item['id'] == $gameCategoryInfo['game_id'] ? "selected" : ""  }}>
+                                    <option value="{{ $item->id }}" {{ $item['id'] == $gameCategoryInfo['game_id'] ? 'selected' : '' }}>
                                         {{ $item->name }}
                                     </option>
                                 @endforeach
                             </select>
                         </div>
-                        <input type="hidden" name="category_id" value="{{ $id }}">
+                        <input name="category_id" type="hidden" value="{{ $id }}">
                         <button class="btn btn-success mt-4" type="submit">Lưu thay đổi</button>
                     </form>
 
@@ -46,12 +38,6 @@
         </div>
 
     </div>
-    <!-- /.container-fluid -->
-
-    </div>
-    <!-- End of Main Content -->
-
-    <!-- End of Content Wrapper -->
     <script>
         // Add the following code if you want the name of the file appear on select
         $(".custom-file-input").on("change", function() {
