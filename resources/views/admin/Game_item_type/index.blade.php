79f4e9e43fb7f556ac23750d3738464a3e387935
nhat191024 <richberchannel01@gmail.com>
2025-03-24 09:18:39 +0700

    feat: replace edit button with link to edit GameItemType

--- resources/views/admin/Game_item_type/index.blade.php
+++ resources/views/admin/Game_item_type/index.blade.php
@@ -35,7 +35,7 @@
                                         <td style="width: 5%;">{{ ++$key }}</td>
                                         <td style="width: 30%;">{{ $item['name'] }}</td>
                                         <td style="width: 20%;">
-                                            <button class="btn btn-primary">Sửa</button>
+                                            <a class="btn btn-primary" href="{{ route('admin.game_item_type.edit', [$item->id]) }}">Sửa</a>
                                             <a class="btn btn-danger" href="{{ route('admin.game_item_type.destroy', [$item->id]) }}">Xóa</a>
                                             <button class="btn btn-primary">Danh sách vật phẩm</button>
                                         </td>
