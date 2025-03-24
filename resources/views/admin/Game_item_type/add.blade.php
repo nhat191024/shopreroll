3998b5243eefe478e1ddaddb2ffacca855719c54
nhat191024 <richberchannel01@gmail.com>
2025-03-24 09:23:43 +0700

    feat: add error handling for form submission in GameItemType add view

--- resources/views/admin/Game_item_type/add.blade.php
+++ resources/views/admin/Game_item_type/add.blade.php
@@ -6,17 +6,27 @@
 
         <div class="card mb-4 shadow">
             <div class="card-body">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        Lôi xảy ra, vui lòng kiểm tra lại thông tin nhập vào
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
                 <div class="table-responsive">
                     <form class="form" action="{{ route('admin.game_item_type.store', [$gameId]) }}" method="post">
                         @csrf
                         <div class="game-item">
                             <div class="form-group mb-3">
                                 <label for="">Tên game item</label>
-                                <input id="" class="form-control" name="game_item[]" type="text" required aria-describedby="game-item-name">
+                                <input id="" class="form-control" name="game_item[]" type="text" aria-describedby="game-item-name">
                             </div>
                         </div>
 
-                        <input type="hidden" name="game_id" value="{{ $gameId }}">
+                        <input name="game_id" type="hidden" value="{{ $gameId }}">
 
                         <button class="btn btn-success mt-4" type="submit">Xác nhận</button>
                         <button id="add-game-item-btn" class="btn btn-success mt-4" type="button">Thêm game item</button>
