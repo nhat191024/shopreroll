e81cc8fdcf1dc676aaace6513a989dc94578d8c8
nhat191024 <richberchannel01@gmail.com>
2025-03-26 02:36:38 +0700

    refactor: update game account routes for improved structure and naming consistency

--- routes/admin/game/account.php
+++ routes/admin/game/account.php
@@ -3,14 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\GameAccountController;
 
-Route::prefix('/game-account')->name('gameAccount.')->group(function () {
-    Route::get('/', [GameAccountController::class, 'index'])->name('index');
-    Route::get('/add', [GameAccountController::class, 'showAddForm'])->name('showAddForm');
-    Route::post('/add', [GameAccountController::class, 'addGameAccount'])->name('add');
-    Route::post('/edit/{id}', [GameAccountController::class, 'editGameAccount'])->name('edit');
-    Route::get('/edit/{id}', [GameAccountController::class, 'showEditForm'])->name('showEditForm');
-    Route::get('/disable/{id}', [GameAccountController::class, 'disableGameAccount'])->name('disable');
-    Route::get('/store/{id}', [GameAccountController::class, 'storeGameAccount'])->name('store');
-    Route::get('/get-game-details/{categoryId}', [GameAccountController::class, 'getGameDetails'])->name('getGameDetails');
-    Route::post('/game-account/import', [GameAccountController::class, 'importFromExcel'])->name('import');
+Route::prefix('/game-account')->name('game_account.')->group(function () {
+    Route::get('/{game}', [GameAccountController::class, 'index'])->name('index');
+    Route::get('/create/{game}', [GameAccountController::class, 'create'])->name('create');
+    Route::post('/store/{game}', [GameAccountController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [GameAccountController::class, 'edit'])->name('edit');
+    Route::post('/update/{id}', [GameAccountController::class, 'update'])->name('update');
+    Route::get('/destroy/{id}', [GameAccountController::class, 'destroy'])->name('destroy');
+    Route::post('/import', [GameAccountController::class, 'excel'])->name('excel');
 });
