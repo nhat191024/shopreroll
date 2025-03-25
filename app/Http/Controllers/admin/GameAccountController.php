7ac04382f197285e36dc17ab36ad3cd4cff89ee0
nhat191024 <richberchannel01@gmail.com>
2025-03-26 02:36:14 +0700

    Refactor: fix controller so it can work with new database

--- app/Http/Controllers/admin/GameAccountController.php
+++ app/Http/Controllers/admin/GameAccountController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Models\Game;
+use App\Models\GameAccount;
+use App\Models\GameCategory;
+
+use App\Http\Requests\StoreGameAccountRequest;
+use App\Http\Requests\UpdateGameAccountRequest;
+
+use Maatwebsite\Excel\Facades\Excel;
+
 use App\Http\Controllers\Controller;
 use App\Imports\GameAccountsImport;
-use Illuminate\Http\Request;
 use App\Service\admin\GameAccountService;
-use Illuminate\Support\Facades\Validator;
-use Maatwebsite\Excel\Facades\Excel;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class GameAccountController extends Controller
 {
@@ -18,145 +28,177 @@ class GameAccountController extends Controller
         $this->gameAccountService = $gameAccountService;
     }
 
-    public function index(Request $request)
+    public function index(Game $game, Request $request)
     {
         $categoryId = $request->get('category_id');
         $status = $request->get('status', 1);
 
-        // Lấy danh sách game accounts với điều kiện lọc
-        $gameAccounts = $this->gameAccountService->getAllGameAccounts($categoryId, $status);
-        $gameCategories = $this->gameAccountService->getAllGameCategories(); // Lấy tất cả danh mục để sử dụng trong form lọc
+        $categories = $game->GameCategory->pluck('name', 'id');
+        $categoryName = $categoryId ? GameCategory::find($categoryId)->name : 'chung';
 
-        return view('admin.game_accounts.index', compact('gameAccounts', 'gameCategories', 'categoryId', 'status'));
-    }
+        $accountsQuery = GameAccount::with(['gameCategory', 'creator'])
+            ->where('game_id', $game->id)
+            ->where('status', $status);
 
-    public function showAddForm()
-    {
-        $gameCategories = $this->gameAccountService->getAllGameCategories();
-        return view('admin.game_accounts.add', compact('gameCategories'));
-    }
-
-    public function addGameAccount(Request $request)
-    {
-        // Validate input data
-        $validator = Validator::make($request->all(), [
-            'title' => 'required|string|max:255',
-            'username' => 'required|string|max:255',
-            'password' => 'required|string|min:6',
-            'server' => 'required|string',
-            'ar' => 'required|integer',
-            'game_category_id' => 'required|exists:game_categories,id',
-            'price_in' => 'nullable|numeric',
-            'price_out' => 'required|numeric',
-            'note' => 'nullable|string',
-            'account_images' => 'required|image',
-            'heroes' => 'required|array',
-            'heroes.*' => 'exists:heros,id',
-            'weapons' => 'required|array',
-            'weapons.*' => 'exists:weapons,id',
-        ]);
-
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator)->withInput();
+        if ($categoryId) {
+            $accountsQuery->where('game_category_id', $categoryId);
         }
 
+        $accounts = $accountsQuery->get();
+
+        return view('admin.game_accounts.index', compact('game', 'categories', 'categoryName', 'accounts'));
+    }
+
+    public function create(Game $game)
+    {
+        $categories = GameCategory::all();
+        $itemTypes = $game->GameItemType;
+        $gameAttributes = $game->GameAttribute->pluck('name', 'id');
+        return view('admin.game_accounts.add', compact('game', 'categories', 'itemTypes', 'gameAttributes'));
+    }
+
+    public function store($game, StoreGameAccountRequest $request)
+    {
+        DB::beginTransaction();
         try {
-            $data = $request->all();
-            $images = $request->file('account_images', []);
-            $this->gameAccountService->addGameAccount($data, $images);
-            return redirect()->route('admin.gameAccount.index')->with('success', 'Thêm tài khoản game thành công');
+            $account = GameAccount::create([
+                'creator_id' => Auth::id(),
+                'game_id' => $game,
+                'game_category_id' => $request->game_category_id,
+                'title' => $request->title,
+                'username' => $request->username,
+                'password' => $request->password,
+                'price_in' => $request->price_in ?? 0,
+                'price_out' => $request->price_out,
+                'note' => $request->note,
+                'status' => 1,
+            ]);
+
+            foreach ($request->game_items as $itemType) {
+                foreach ($itemType as $item) {
+                    $account->AccountItem()->create([
+                        'game_item_id' => $item,
+                    ]);
+                }
+            }
+
+            foreach ($request->game_attributes as $attributeId => $attribute) {
+                $account->AccountAttribute()->create([
+                    'game_attribute_id' => $attributeId,
+                    'value' => $attribute[0],
+                ]);
+            }
+
+            if ($request->hasFile('account_images')) {
+                $path = 'image/accounts/';
+                foreach ($request->file('account_images') as $image) {
+                    $filename = uniqid() . '_' . time() . $image->getClientOriginalExtension();
+                    $image->move(public_path($path), $filename);
+
+                    $account->AccountImage()->create([
+                        'image' => $path . '/' . $filename
+                    ]);
+                }
+            }
+
+            DB::commit();
+            return redirect()->route('admin.game_account.index', $game)->with('success', 'Thêm tài khoản game thành công');
         } catch (\Exception $e) {
-            return redirect()->back()->with('error', 'Lỗi: ' . $e->getMessage())->withInput();
-        }
-    }
-
-    public function editGameAccount($id, Request $request)
-    {
-        // Validate input data
-        $validator = Validator::make($request->all(), [
-            'title' => 'required|string|max:255',
-            'username' => 'required|string|max:255',
-            'password' => 'required|string|min:6',
-            'server' => 'required|string',
-            'ar' => 'required|integer',
-            'game_category_id' => 'required|exists:game_categories,id',
-            'price_in' => 'nullable|numeric',
-            'price_out' => 'required|numeric',
-            'note' => 'nullable|string',
-            'account_image' => 'nullable|image',
-            'heroes' => 'required|array',
-            'heroes.*' => 'exists:heros,id',
-            'weapons' => 'required|array',
-            'weapons.*' => 'exists:weapons,id',
-        ]);
-
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator)->withInput();
-        }
-
-        try {
-            $data = $request->all();
-            $images = $request->file('account_image'); // Lấy ảnh từ Request
-            $this->gameAccountService->editGameAccount($id, $data, $images); // Chuyển dữ liệu sang Service
-            return redirect()->route('admin.gameAccount.index')->with('success', 'Cập nhật tài khoản game thành công');
-        } catch (\Exception $e) {
-            return redirect()->back()->with('error', 'Lỗi: ' . $e->getMessage())->withInput();
-        }
-    }
-
-
-    public function showEditForm($id)
-    {
-        $gameAccount = $this->gameAccountService->getGameAccountById($id);
-        $gameCategories = $this->gameAccountService->getAllGameCategories();
-
-        // Lấy các hero và weapon đã chọn để đổ ra form edit
-        $selectedHeroes = $gameAccount->AccountHero->pluck('hero_id')->toArray();
-        $selectedWeapons = $gameAccount->AccountWeapon->pluck('weapon_id')->toArray();
-        // Lấy tất cả heroes và weapons có liên quan đến danh mục game được chọn
-        $gameCategoryId = $gameAccount->game_category_id;
-        $gameDetails = $this->gameAccountService->getGameDetails($gameCategoryId);
-        $heroes = $gameDetails['heroes'];
-        $weapons = $gameDetails['weapons'];
-
-        return view('admin.game_accounts.edit', compact(
-            'gameAccount',
-            'gameCategories',
-            'heroes',
-            'weapons',
-            'selectedHeroes',
-            'selectedWeapons'
-        ));
-    }
-
-
-    public function disableGameAccount($id)
-    {
-        try {
-            $this->gameAccountService->disableGameAccount($id);
-            return redirect()->route('admin.gameAccount.index')->with('success', 'Tài khoản game đã bị vô hiệu hóa');
-        } catch (\Exception $e) {
+            DB::rollBack();
             return redirect()->back()->with('error', 'Lỗi: ' . $e->getMessage());
         }
     }
 
-    public function storeGameAccount($id)
+    public function edit($id)
     {
+        $account = GameAccount::findOrFail($id)->load('AccountItem', 'AccountAttribute', 'AccountImage');
+        $game = $account->Game;
+        $categories = GameCategory::all();
+        $itemTypes = $game->GameItemType;
+        $accountItems = $account->AccountItem->pluck('game_item_id')->toArray();
+        $gameAttributes = $game->GameAttribute->pluck('name', 'id');
+        return view('admin.game_accounts.edit', compact('game', 'account', 'categories', 'itemTypes', 'accountItems', 'gameAttributes'));
+    }
+
+    public function update($account, UpdateGameAccountRequest $request)
+    {
+        DB::beginTransaction();
         try {
-            $this->gameAccountService->storeGameAccount($id);
-            return redirect()->route('admin.gameAccount.index')->with('success', 'Tài khoản game đã được khôi phục');
+            $account = GameAccount::findOrFail($account);
+
+            $account->update([
+                'game_category_id' => $request->game_category_id,
+                'title' => $request->title,
+                'username' => $request->username,
+                'password' => $request->password,
+                'price_in' => $request->price_in ?? 0,
+                'price_out' => $request->price_out,
+                'note' => $request->note,
+            ]);
+
+            $account->AccountAttribute()->delete();
+            $account->AccountItem()->delete();
+
+            foreach ($request->game_items as $itemType) {
+                foreach ($itemType as $item) {
+                    $account->AccountItem()->create([
+                        'game_item_id' => $item,
+                    ]);
+                }
+            }
+
+            foreach ($request->game_attributes as $attributeId => $attribute) {
+                $account->AccountAttribute()->create([
+                    'game_attribute_id' => $attributeId,
+                    'value' => $attribute[0],
+                ]);
+            }
+
+            if ($request->hasFile('account_images')) {
+                foreach ($account->AccountImage as $image) {
+                    $path = public_path($image->image);
+                    if (file_exists($path)) {
+                        unlink($path);
+                    }
+                }
+                $account->AccountImage()->delete();
+
+                $path = 'image/accounts/';
+                foreach ($request->file('account_images') as $image) {
+                    $filename = uniqid() . '_' . time() . '.' . $image->getClientOriginalExtension();
+                    $image->move(public_path($path), $filename);
+
+                    $account->AccountImage()->create([
+                        'image' => $path . $filename
+                    ]);
+                }
+            }
+
+            DB::commit();
+            return redirect()->route('admin.game_account.index', $account->game_id)->with('success', 'Cập nhật tài khoản game thành công');
         } catch (\Exception $e) {
-            return redirect()->back()->with('error', 'Lỗi: ' . $e->getMessage());
+            DB::rollBack();
+            return redirect()->back()->with('error', 'Lỗi: ' . $e->getMessage())->withInput();
         }
     }
 
-    public function getGameDetails($categoryId)
+    public function destroy($id)
     {
         try {
-            $result = $this->gameAccountService->getGameDetails($categoryId);
-            return response()->json(['success' => true, 'data' => $result], 200);
+            $account = GameAccount::findOrFail($id);
+            $account->AccountAttribute()->delete();
+            $account->AccountItem()->delete();
+            foreach ($account->AccountImage as $image) {
+                $path = public_path($image->image);
+                if (file_exists($path)) {
+                    unlink($path);
+                }
+            }
+            $account->AccountImage()->delete();
+            $account->delete();
+            return redirect()->back()->with('success', 'Xóa tài khoản game thành công');
         } catch (\Exception $e) {
-            return response()->json(['success' => false, 'message' => 'Lỗi: ' . $e->getMessage()], 500);
+            return redirect()->back()->with('error', 'Lỗi: ' . $e->getMessage());
         }
     }
 
