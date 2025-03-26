45d29483f35b34821a77f6e4f848f269c5714351
nhat191024 <richberchannel01@gmail.com>
2025-03-26 19:26:20 +0700

    refactor: enhance GameAccountsImport model method for improved data handling and validation

--- app/Imports/GameAccountsImport.php
+++ app/Imports/GameAccountsImport.php
@@ -3,70 +3,69 @@
 namespace App\Imports;
 
 use App\Models\GameAccount;
-use App\Models\AccountHero;
-use App\Models\AccountWeapon;
-use Illuminate\Support\Facades\Storage;
+
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class GameAccountsImport implements ToModel, WithStartRow
 {
     protected $errors = []; // Khai báo biến $errors
 
+    // Bắt đầu đọc từ dòng thứ 2
     public function startRow(): int
     {
-        return 2; // Bắt đầu đọc từ dòng thứ 2
+        return 2;
     }
 
     public function model(array $row)
     {
+        DB::beginTransaction();
         try {
-            // Kiểm tra dữ liệu từng dòng
             $this->validateRow($row);
 
-            // Lưu ảnh vào thư mục nếu có
+            $accountImages = json_decode($row[8], true);
+            $accountItems = json_decode($row[9], true);
+            $accountAttributes = json_decode($row[10], true);
 
-            // Thêm tài khoản game
             $gameAccount = GameAccount::create([
-                'title' => $row[0],
-                'username' => $row[1],
-                'password' => bcrypt($row[2]),
-                'server' => $row[3],
-                'AR' => (int) $row[4],
-                'game_category_id' => (int) $row[5],
-                'price_in' => isset($row[6]) ? (float) $row[6] : 0,
-                'price_out' => (float) $row[7],
-                'note' => $row[8] ?? null,
+                'creator_id' => Auth::id(),
+                'game_id' => $row[0],
+                'game_category_id' => (int) $row[1],
+                'title' => $row[2],
+                'username' => $row[3],
+                'password' => $row[4],
+                'price_in' => isset($row[5]) ? (float) $row[5] : 0,
+                'price_out' => (float) $row[6],
+                'note' => $row[7] ?? null,
                 'status' => 1,
-                'creator_id' => 1, // ID mặc định cho creator
-                'account_image' => $row[11] ?? null, // Lưu đường dẫn ảnh vào cột account_image
             ]);
 
-            // Xử lý tướng
-            if (!empty($row[9])) {
-                $heroIds = explode(',', $row[9]);
-                foreach ($heroIds as $heroId) {
-                    AccountHero::create([
-                        'account_id' => $gameAccount->id,
-                        'hero_id' => trim($heroId),
-                    ]);
-                }
+            foreach ($accountImages as $image) {
+                $gameAccount->images()->create(['image' => $image]);
             }
 
-            // Xử lý vũ khí
-            if (!empty($row[10])) {
-                $weaponIds = explode(',', $row[10]);
-                foreach ($weaponIds as $weaponId) {
-                    AccountWeapon::create([
-                        'account_id' => $gameAccount->id,
-                        'weapon_id' => trim($weaponId),
-                    ]);
-                }
+            foreach ($accountItems as $item) {
+                $gameAccount->items()->create([
+                    'game_account_id' => $gameAccount->id,
+                    'game_item_id' => $item
+                ]);
+            }
+            foreach ($accountAttributes as $id => $attribute) {
+                $gameAccount->attributes()->create([
+                    'game_account_id' => $gameAccount->id,
+                    'game_attribute_id' => $id,
+                    'value' => $attribute
+                ]);
             }
 
-            return $gameAccount;
+            DB::commit();
         } catch (\Exception $e) {
+            DB::rollBack();
             $this->errors[] = "Dòng lỗi: " . json_encode($row) . " - " . $e->getMessage();
             return null;
         }
@@ -75,23 +74,28 @@ class GameAccountsImport implements ToModel, WithStartRow
     private function validateRow($row)
     {
         $validator = Validator::make([
-            'title' => $row[0],
-            'username' => $row[1],
-            'password' => $row[2],
-            'server' => $row[3],
-            'ar' => $row[4],
-            'game_category_id' => $row[5],
-            'price_in' => $row[6],
-            'price_out' => $row[7],
+            'game_id' => $row[0],
+            'game_category_id' => $row[1],
+            'title' => $row[2],
+            'username' => $row[3],
+            'password' => $row[4],
+            'price_in' => $row[5],
+            'price_out' => $row[6],
+            'note' => $row[7],
+            'account_image' => $row[8],
+            'account_items' => $row[9],
+            'account_attributes' => $row[10],
         ], [
             'title' => 'required|string|max:255',
-            'username' => 'required|string|max:255',
-            'password' => 'required|string|min:6',
-            'server' => 'required|string',
-            'ar' => 'required|integer|min:0',
             'game_category_id' => 'required|integer|exists:game_categories,id',
-            'price_in' => 'nullable|numeric|min:0',
-            'price_out' => 'required|numeric|min:0',
+            'username' => 'required|string|max:255',
+            'password' => 'required|string|max:255',
+            'price_in' => 'nullable|integer',
+            'price_out' => 'required|integer',
+            'note' => 'nullable|string',
+            'account_image' => 'required|string',
+            'account_items' => 'required|string',
+            'account_attributes' => 'required|string',
         ]);
 
         if ($validator->fails()) {
