0468beeb6fcdb4a9e1d416225e0a0a4e3280b1b9
nhat191024 <richberchannel01@gmail.com>
2025-03-27 02:24:42 +0700

    refactor: simplify route for game category index by removing redundant 'list' segment

--- routes/admin/game/category.php
+++ routes/admin/game/category.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\GameCategoryController;
 
 Route::prefix('/game-category')->name('GameCategory.')->group(function () {
-    Route::get('/list/{id}', [GameCategoryController::class, 'index'])->name('index');
+    Route::get('/{id}', [GameCategoryController::class, 'index'])->name('index');
     Route::get('/add', [GameCategoryController::class, 'showAddCategory'])->name('showAdd');
     Route::post('/add', [GameCategoryController::class, 'addCategory'])->name('add');
     Route::post('/edit', [GameCategoryController::class, 'editCategory'])->name('edit');
