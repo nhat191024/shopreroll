e06e49ef1d5814b8f3b491621e7a27881d8704cd
nhat191024 <richberchannel01@gmail.com>
2025-03-27 03:40:38 +0700

    refactor: simplify markup and improve form input structure in editRerollKey view

--- resources/views/admin/RerollKey/editRerollKey.blade.php
+++ resources/views/admin/RerollKey/editRerollKey.blade.php
@@ -1,25 +1,17 @@
 @extends('admin.master')
 @section('main')
-    <!-- Content Wrapper -->
-
-    <!-- Begin Page Content -->
     <div class="container-fluid">
-
-        <!-- Page Heading -->
         <h1 class="h3 mb-2 text-gray-800">Sửa Reroll Key</h1>
-        <!-- DataTales Example -->
-        <div class="card shadow mb-4">
+        <div class="card mb-4 shadow">
             <div class="card-body">
                 <div class="table-responsive">
                     <form action="{{ route('admin.rerollKey.edit', ['idPackage' => $idPackage, 'idKey' => $idKey]) }}" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group">
                             <label for="keyInput">Tên Reroll Key</label>
-                            <input maxlength="255" required type="text" class="form-control" id="keyInput"
-                                name="key" value="{{ old('key', $rerollKeyInfo->key ?? '') }}"
-                                placeholder="Nhập key">
+                            <input id="keyInput" class="form-control" name="key" type="text" required value="{{ old('key', $rerollKeyInfo->key ?? '') }}" maxlength="255" placeholder="Nhập key">
                         </div>
-                        <input type="hidden" value="{{ $rerollKeyInfo->id }}" name="id">
+                        <input name="id" type="hidden" value="{{ $rerollKeyInfo->id }}">
                         <a class="btn btn-primary mt-4" onclick="history.back()">Quay lại</a>
                         <button id="saveEdit" class="btn btn-success mt-4" type="submit">Lưu</button>
                     </form>
@@ -28,10 +20,4 @@
         </div>
 
     </div>
-    <!-- /.container-fluid -->
-
-    </div>
-    <!-- End of Main Content -->
-
-    <!-- End of Content Wrapper -->
 @endsection
