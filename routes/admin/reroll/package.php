97117c69a9900841269b3f82e7097a0321e48fab
nhat191024 <richberchannel01@gmail.com>
2025-03-27 16:19:44 +0700

    fix: update RerollPackage routes for improved clarity and consistency in naming and structure

--- routes/admin/reroll/package.php
+++ routes/admin/reroll/package.php
@@ -3,14 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\RerollPackageController;
 
-Route::prefix('/reroll-package')->name('RerollPackage.')->group(function () {
-    Route::get('/', [RerollPackageController::class, 'index'])->name('index');
-    Route::get('/add', [RerollPackageController::class, 'showAddRerollPackage'])->name('showAdd');
-    Route::post('/add', [RerollPackageController::class, 'addRerollPackage'])->name('add');
-    Route::post('/edit', [RerollPackageController::class, 'editRerollPackage'])->name('edit');
-    Route::get('/edit/{id}', [RerollPackageController::class, 'showEditRerollPackage'])->name('showEdit');
-    Route::get('/delete/{id}', [RerollPackageController::class, 'deleteRerollPackage'])->name('delete');
-    Route::get('/detail/{id}', [RerollPackageController::class, 'detailRerollPackage'])->name('detail');
-
-    require __DIR__ . '/subCategory.php';
+Route::prefix('/reroll-package')->name('rerollPackage.')->group(function () {
+    Route::get('/{subCategory}', [RerollPackageController::class, 'index'])->name('index');
+    Route::get('/create/form', [RerollPackageController::class, 'create'])->name('create');
+    Route::post('/store', [RerollPackageController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [RerollPackageController::class, 'edit'])->name('edit');
+    Route::post('/update/{id}', [RerollPackageController::class, 'update'])->name('update');
+    Route::get('/destroy/{id}', [RerollPackageController::class, 'destroy'])->name('destroy');
 });
