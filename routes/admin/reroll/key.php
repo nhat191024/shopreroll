40b9120beeef0edbd07293a43cbe2e1c950528fc
nhat191024 <richberchannel01@gmail.com>
2025-03-27 16:47:59 +0700

    fix: update RerollKey routes for improved clarity and consistency in naming and structure

--- routes/admin/reroll/key.php
+++ routes/admin/reroll/key.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\RerollKeyController;
 
-Route::prefix('/detail/{idPackage}/reroll-key')->name('rerollKey.')->group(function () {
-    Route::get('/', [RerollKeyController::class, 'index'])->name('index');
-    Route::get('/add', [RerollKeyController::class, 'showAddRerollKey'])->name('showAdd');
-    Route::post('/add', [RerollKeyController::class, 'addRerollKey'])->name('add');
-    Route::post('/edit', [RerollKeyController::class, 'editRerollKey'])->name('edit');
-    Route::get('/edit/{idKey}', [RerollKeyController::class, 'showEditRerollKey'])->name('ShowEdit');
-    Route::get('/delete/{idKey}', [RerollKeyController::class, 'deleteRerollPackage'])->name('delete');
+Route::prefix('/reroll-key')->name('rerollKey.')->group(function () {
+    Route::get('/{id}', [RerollKeyController::class, 'index'])->name('index');
+    Route::get('/create/{package}', [RerollKeyController::class, 'create'])->name('create');
+    Route::post('/store', [RerollKeyController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [RerollKeyController::class, 'edit'])->name('edit');
+    Route::post('/update/{id}', [RerollKeyController::class, 'update'])->name('update');
+    Route::get('/destroy/{idKey}', [RerollKeyController::class, 'destroy'])->name('destroy');
 });
