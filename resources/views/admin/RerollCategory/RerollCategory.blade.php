5832312f7bc29cd935c4328ab76a8ca8bdc490f0
nhat191024 <richberchannel01@gmail.com>
2025-03-27 14:41:46 +0700

    fix: update route names and improve table headers in RerollCategory view

--- resources/views/admin/RerollCategory/RerollCategory.blade.php
+++ resources/views/admin/RerollCategory/RerollCategory.blade.php
@@ -5,7 +5,7 @@
 
         <div class="card mb-4 shadow">
             <div class="card-header py-3">
-                <a class="btn btn-primary" href="{{ route('admin.RerollCategory.showAdd') }}">Thêm Reroll Category</a>
+                <a class="btn btn-primary" href="{{ route('admin.rerollCategory.create') }}">Thêm Reroll Category</a>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
@@ -22,45 +22,54 @@
                     <table id="dataTable" class="table-bordered table" width="100%" cellspacing="0">
                         <thead>
                             <tr>
-                                <th>Mã RC</th>
-                                <th class="col-2">Tên Reroll Category</th>
-                                <th class="col-3">Ghi chú</th>
+                                <th>STT</th>
+                                <th>Tên danh mục reroll</th>
+                                <th>Ghi chú</th>
                                 <th>Ảnh</th>
+                                <th>Trạng thái</th>
                                 <th>Chức năng</th>
                             </tr>
                         </thead>
-                        <tfoot>
-                            <tr>
-                                <th>Mã RC</th>
-                                <th class="col-2">Tên Reroll Category</th>
-                                <th class="col-3">Ghi chú</th>
-                                <th>Ảnh</th>
-                                <th>Chức năng</th>
-                            </tr>
-                        </tfoot>
                         <tbody>
-                            @foreach ($allRerollCategory as $key => $item)
+                            @foreach ($rerollCategories as $key => $item)
                                 <tr>
-                                    <td>{{ $item['id'] }}</td>
-                                    <td>{{ $item['name'] }}</td>
-                                    <td>{{ $item['note'] }}</td>
-                                    <td class="text-center"><img width="200px" src="{{ asset('image/thumb/' . $item['image']) }}" alt=""></td>
+                                    <td>{{ ++$key }}</td>
+                                    <td>{{ $item->name }}</td>
+                                    <td>{{ $item->note }}</td>
+                                    <td class="text-center"><img width="200px" src="{{ asset($item->image) }}" alt=""></td>
+                                    <td class="text-center">
+                                        @if ($item->status == 1)
+                                            <span class="badge badge-success px-4 py-2">Hiện</span>
+                                        @else
+                                            <span class="badge badge-danger px-4 py-2">Ẩn</span>
+                                        @endif
+                                    </td>
                                     <td class="text-center">
                                         @if ($item->status == 0)
-                                            <a class="btn btn-success" onclick="event.preventDefault(); if (confirm('Bạn chắc chắn muốn hiện item {{ $item->name }} chứ?')) { window.location.href = '{{ route('admin.RerollCategory.ChangeStatus', $item->id) }}'; }">
+                                            <a class="btn btn-success" onclick="event.preventDefault(); if (confirm('Bạn chắc chắn muốn hiện item {{ $item->name }} chứ?')) { window.location.href = '{{ route('admin.rerollCategory.ChangeStatus', $item->id) }}'; }">
                                                 Hiện </a>
                                         @else
-                                            <a class="btn btn-danger" onclick="event.preventDefault(); if (confirm('Bạn chắc chắn muốn ẩn item {{ $item->name }} chứ?')) { window.location.href = '{{ route('admin.RerollCategory.ChangeStatus', $item->id) }}'; }">
+                                            <a class="btn btn-danger" onclick="event.preventDefault(); if (confirm('Bạn chắc chắn muốn ẩn item {{ $item->name }} chứ?')) { window.location.href = '{{ route('admin.rerollCategory.ChangeStatus', $item->id) }}'; }">
                                                 Ẩn </a>
                                         @endif
-                                        <a class="btn btn-info" href="{{ route('admin.RerollCategory.ShowEdit', $item->id) }}">Sửa</a>
-                                        <a class="btn btn-info" href="{{ route('admin.RerollCategory.Detail', $item->id) }}">
+                                        <a class="btn btn-info" href="{{ route('admin.rerollCategory.edit', $item->id) }}">Sửa</a>
+                                        <a class="btn btn-info" href="{{ route('admin.rerollSubCategory.index', $item->id) }}">
                                             Chi tiết
                                         </a>
                                     </td>
                                 </tr>
                             @endforeach
                         </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>STT</th>
+                                <th>Tên danh mục reroll</th>
+                                <th>Ghi chú</th>
+                                <th>Ảnh</th>
+                                <th>Trạng thái</th>
+                                <th>Chức năng</th>
+                            </tr>
+                        </tfoot>
                     </table>
                 </div>
             </div>
