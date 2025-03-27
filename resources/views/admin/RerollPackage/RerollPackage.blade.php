be2e274d4078b4654b138a07852d20b2c39ffbba
nhat191024 <richberchannel01@gmail.com>
2025-03-27 16:19:39 +0700

    fix: update RerollPackage view for improved clarity and consistency in naming and structure

--- resources/views/admin/RerollPackage/RerollPackage.blade.php
+++ resources/views/admin/RerollPackage/RerollPackage.blade.php
@@ -1,10 +1,10 @@
 @extends('admin.master')
 @section('main')
     <div class="container-fluid">
-        <h1 class="h3 mb-2 text-gray-800">Danh sách Reroll Package</h1>
+        <h1 class="h3 mb-2 text-gray-800">Danh sách gói reroll - {{ $subCategoryName }}</h1>
         <div class="card mb-4 shadow">
             <div class="card-header py-3">
-                <a class="btn btn-primary" href="{{ route('admin.RerollPackage.showAdd') }}">Thêm Reroll Package</a>
+                <a class="btn btn-primary" href="{{ route('admin.rerollPackage.create') }}">Thêm gói reroll</a>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
@@ -21,40 +21,37 @@
                     <table id="dataTable" class="table-bordered table" width="100%" cellspacing="0">
                         <thead>
                             <tr>
-                                <th>Mã</th>
-                                <th class="col-2">Tên</th>
-                                <th class="col-3">Giá</th>
-                                <th class="col-4">Chức năng</th>
+                                <th>STT</th>
+                                <th>Tên</th>
+                                <th>Giá</th>
+                                <th>Chức năng</th>
                             </tr>
                         </thead>
-                        <tfoot>
-                            <tr>
-                                <th>Mã</th>
-                                <th class="col-2">Tên</th>
-                                <th class="col-3">Giá</th>
-                                <th class="col-4">Chức năng</th>
-                            </tr>
-                        </tfoot>
                         <tbody>
-                            @foreach ($allRerollPackagies as $key => $item)
+                            @foreach ($packages as $key => $item)
                                 <tr>
-                                    <td>{{ $item['id'] }}</td>
-                                    <td>{{ $item['name'] }}</td>
-                                    <td>{{ $item['price'] }}</td>
+                                    <td>{{ ++$key }}</td>
+                                    <td>{{ $item->name }}</td>
+                                    <td>{{ number_format($item->price) }} VNĐ</td>
                                     <td class="text-center">
-                                        <a class="btn btn-danger" onclick="event.preventDefault(); if (confirm('Bạn chắc chắn muốn ẩn item {{ $item->name }} chứ?')) { window.location.href = '{{ route('admin.RerollPackage.delete', $item->id) }}'; }">
-                                            Xóa </a>
-                                        <a class="btn btn-info" href="{{ route('admin.RerollPackage.showEdit', $item->id) }}">Sửa</a>
-                                        <a class="btn btn-info" href="{{ route('admin.RerollPackage.detail', $item->id) }}">Chi tiết</a>
+                                        <a class="btn btn-danger" onclick="event.preventDefault(); if (confirm('Bạn chắc chắn muốn ẩn item {{ $item->name }} chứ?')) { window.location.href = '{{ route('admin.rerollPackage.destroy', $item->id) }}'; }">Xóa</a>
+                                        <a class="btn btn-info" href="{{ route('admin.rerollPackage.edit', $item->id) }}">Sửa</a>
+                                        <a class="btn btn-info" href="{{ route('admin.rerollKey.index', $item->id) }}">Chi tiết</a>
                                     </td>
-
                                 </tr>
                             @endforeach
                         </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>Mã</th>
+                                <th>Tên</th>
+                                <th>Giá</th>
+                                <th>Chức năng</th>
+                            </tr>
+                        </tfoot>
                     </table>
                 </div>
             </div>
         </div>
-
     </div>
 @endsection
