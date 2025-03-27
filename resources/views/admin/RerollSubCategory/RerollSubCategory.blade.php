8bf6168c2ec399e61e763e4c21ff6e4afa306bcf
nhat191024 <richberchannel01@gmail.com>
2025-03-27 22:31:27 +0700

    fix: update RerollSubCategory view header for improved clarity in naming

--- resources/views/admin/RerollSubCategory/RerollSubCategory.blade.php
+++ resources/views/admin/RerollSubCategory/RerollSubCategory.blade.php
@@ -2,7 +2,7 @@
 @section('main')
     <div class="container-fluid">
         @if ($category == 0)
-            <h1 class="h3 mb-2 text-gray-800">Danh sách danh mục reroll</h1>
+            <h1 class="h3 mb-2 text-gray-800">Danh sách danh mục reroll phụ</h1>
         @else
             <h1 class="h3 mb-2 text-gray-800">Danh sách danh mục reroll phụ - {{ $categoryName }}</h1>
         @endif
