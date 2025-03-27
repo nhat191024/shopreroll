3d7453adc7035a2264b8f9897c1febf06d1c1b09
nhat191024 <richberchannel01@gmail.com>
2025-03-27 16:48:24 +0700

    fix: enhance add RerollKey view by improving form structure and adding package selection

--- resources/views/admin/RerollKey/addRerollKey.blade.php
+++ resources/views/admin/RerollKey/addRerollKey.blade.php
@@ -5,11 +5,19 @@
         <div class="card mb-4 shadow">
             <div class="card-body">
                 <div class="table-responsive">
-                    <form action="{{ route('admin.rerollKey.add', ['idPackage' => $idPackage]) }}" method="post" enctype="multipart/form-data">
+                    <form action="{{ route('admin.rerollKey.store') }}" method="post">
                         @csrf
                         <div class="form-group">
-                            <label for="keyName">Tên Reroll Key</label>
-                            <input id="keyName" class="form-control" name="key" type="text" required maxlength="255" placeholder="Nhập tên Reroll Key">
+                            <label for="keyName">Reroll Key</label>
+                            <input id="keyName" class="form-control" name="key" type="text" required maxlength="255" placeholder="Nhập Reroll Key">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Gói reroll</label>
+                            <select class="form-control" name="package_id" required>
+                                @foreach ($rerollPackage as $id => $item)
+                                    <option value="{{ $id }}" @selected($id == $package)>{{ $item }}</option>
+                                @endforeach
+                            </select>
                         </div>
                         <a class="btn btn-primary mt-4" onclick="history.back()">Quay lại</a>
                         <button id="saveAdd" class="btn btn-success mt-4" type="submit">Lưu</button>
