e37e036af347447d48d2a7f431ceaff122740fac
nhat191024 <richberchannel01@gmail.com>
2025-03-27 16:48:28 +0700

    fix: update edit RerollKey view to correct form action and add package selection

--- resources/views/admin/RerollKey/editRerollKey.blade.php
+++ resources/views/admin/RerollKey/editRerollKey.blade.php
@@ -5,15 +5,22 @@
         <div class="card mb-4 shadow">
             <div class="card-body">
                 <div class="table-responsive">
-                    <form action="{{ route('admin.rerollKey.edit', ['idPackage' => $idPackage, 'idKey' => $idKey]) }}" method="post" enctype="multipart/form-data">
+                    <form action="{{ route('admin.rerollKey.update', $key->id) }}" method="post">
                         @csrf
                         <div class="form-group">
                             <label for="keyInput">Tên Reroll Key</label>
-                            <input id="keyInput" class="form-control" name="key" type="text" required value="{{ old('key', $rerollKeyInfo->key ?? '') }}" maxlength="255" placeholder="Nhập key">
+                            <input id="keyInput" class="form-control" name="key" type="text" required value="{{ old('key', $key->key ?? '') }}" maxlength="255" placeholder="Nhập key">
                         </div>
-                        <input name="id" type="hidden" value="{{ $rerollKeyInfo->id }}">
+                        <div class="form-group">
+                            <label for="">Gói reroll</label>
+                            <select class="form-control" name="package_id" required>
+                                @foreach ($rerollPackage as $id => $item)
+                                    <option value="{{ $id }}" @selected($id == $key->reroll_package_id)>{{ $item }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                         <a class="btn btn-primary mt-4" onclick="history.back()">Quay lại</a>
-                        <button id="saveEdit" class="btn btn-success mt-4" type="submit">Lưu</button>
+                        <button class="btn btn-success mt-4" type="submit">Lưu</button>
                     </form>
                 </div>
             </div>
