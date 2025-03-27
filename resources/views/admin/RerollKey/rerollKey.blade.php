7bdfba33f9bd57b1df418dc88401355cd0c7baeb
nhat191024 <richberchannel01@gmail.com>
2025-03-27 16:47:53 +0700

    feat: add RerollKey view for managing reroll keys with CRUD functionality

--- resources/views/admin/RerollKey/rerollKey.blade.php
+++ resources/views/admin/RerollKey/rerollKey.blade.php
@@ -1,13 +1,10 @@
 @extends('admin.master')
 @section('main')
     <div class="container-fluid">
-
-        <!-- Page Heading -->
-        <h1 class="h3 mb-2 text-gray-800">Danh sách Reroll Key</h1>
-        <!-- DataTales Example -->
+        <h1 class="h3 mb-2 text-gray-800">Danh sách Reroll Key - {{ $packageName }}</h1>
         <div class="card mb-4 shadow">
             <div class="card-header py-3">
-                <a class="btn btn-primary" href="{{ route('admin.rerollKey.showAdd', ['idPackage' => $idPackage]) }}">Thêm Reroll Key</a>
+                <a class="btn btn-primary" href="{{ route('admin.rerollKey.create', $idPackage) }}">Thêm Reroll Key - {{ $packageName }}</a>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
@@ -25,34 +22,32 @@
                         <thead>
                             <tr>
                                 <th>ID</th>
-                                <th class="col-4">Key</th>
-                                <th class="col-3 text-center">Hành động</th>
+                                <th>Key</th>
+                                <th>Hành động</th>
                             </tr>
                         </thead>
-                        <tfoot>
-                            <tr>
-                                <th>ID</th>
-                                <th class="col-4">Key</th>
-                                <th class="col-3 text-center">Hành động</th>
-                            </tr>
-                        </tfoot>
                         <tbody>
-                            @foreach ($allRerollKeys as $key => $item)
+                            @foreach ($rerollKeys as $key => $item)
                                 <tr>
-                                    <td>{{ $item->id }}</td>
+                                    <td style="width: 5%">{{ ++$key }}</td>
                                     <td>{{ $item->key }}</td>
                                     <td class="text-center">
-                                        <a class="btn btn-danger" onclick="event.preventDefault(); if (confirm('Bạn chắc chắn muốn ẩn item {{ $item->id }} chứ?')) { window.location.href = '{{ route('admin.rerollKey.delete', ['idPackage' => $idPackage, 'idKey' => $item->id]) }}'; }">
-                                            Xóa </a>
-                                        <a class="btn btn-info" href="{{ route('admin.rerollKey.ShowEdit', ['idPackage' => $idPackage, 'idKey' => $item->id]) }}">Sửa</a>
+                                        <a class="btn btn-danger" onclick="event.preventDefault(); if (confirm('Bạn chắc chắn muốn xóa key {{ $item->id }} chứ?')) { window.location.href = '{{ route('admin.rerollKey.destroy', $item->id) }}'; }">Xóa</a>
+                                        <a class="btn btn-info" href="{{ route('admin.rerollKey.edit', $item->id) }}">Sửa</a>
                                     </td>
                                 </tr>
                             @endforeach
                         </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>ID</th>
+                                <th>Key</th>
+                                <th>Hành động</th>
+                            </tr>
+                        </tfoot>
                     </table>
                 </div>
             </div>
         </div>
-
     </div>
 @endsection
