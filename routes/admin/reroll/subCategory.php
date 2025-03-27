de9598a244bb356559d7f1df2b6c72caea3b1954
nhat191024 <richberchannel01@gmail.com>
2025-03-27 15:36:31 +0700

    fix: update reroll sub-category routes for improved clarity and consistency

--- routes/admin/reroll/subCategory.php
+++ routes/admin/reroll/subCategory.php
@@ -4,11 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\RerollSubCategoryController;
 
 Route::prefix('/reroll-sub-category')->name('rerollSubCategory.')->group(function () {
-    Route::get('/', [RerollSubCategoryController::class, 'index'])->name('index');
-    Route::get('/add', [RerollSubCategoryController::class, 'showAddRerollSubCategory'])->name('showAdd');
-    Route::post('/add', [RerollSubCategoryController::class, 'addRerollSubCategory'])->name('add');
-    Route::post('/edit', [RerollSubCategoryController::class, 'editRerollSubCategory'])->name('edit');
-    Route::get('/edit/{id}', [RerollSubCategoryController::class, 'showEditRerollSubCategory'])->name('ShowEdit');
-    Route::get('/detail/{id}', [RerollSubCategoryController::class, 'detailRerollSubCategory'])->name('Detail');
-    Route::get('/ChangeStatus/{id}', [RerollSubCategoryController::class, 'ChangeCategoryStatus'])->name('ChangeStatus');
+    Route::get('/{category}', [RerollSubCategoryController::class, 'index'])->name('index');
+    Route::get('/create/form', [RerollSubCategoryController::class, 'create'])->name('create');
+    Route::post('/store', [RerollSubCategoryController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [RerollSubCategoryController::class, 'edit'])->name('edit');
+    Route::post('/update/{id}', [RerollSubCategoryController::class, 'update'])->name('update');
+    Route::get('/ChangeStatus/{id}', [RerollSubCategoryController::class, 'changeCategoryStatus'])->name('ChangeStatus');
 });
