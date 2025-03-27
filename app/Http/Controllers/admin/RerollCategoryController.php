0a91a2f1c5e94120d407e31faadbeca9306aa243
nhat191024 <richberchannel01@gmail.com>
2025-03-27 14:42:18 +0700

    fix: refactor RerollCategoryController to use Eloquent model and improve method naming

--- app/Http/Controllers/admin/RerollCategoryController.php
+++ app/Http/Controllers/admin/RerollCategoryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Models\RerollCategory;
+
 use App\Http\Controllers\Controller;
 use App\Service\admin\RerollCategoryService;
+
 use Illuminate\Http\Request;
 
 class RerollCategoryController extends Controller
@@ -17,16 +20,16 @@ class RerollCategoryController extends Controller
 
     public function index()
     {
-        $allRerollCategory = $this->rerollCategoryService->getAll();
-        return view('admin.RerollCategory.RerollCategory', compact('allRerollCategory'));
+        $rerollCategories = RerollCategory::all();
+        return view('admin.RerollCategory.RerollCategory', compact('rerollCategories'));
     }
 
-    public function showAddRerollCategory()
+    public function create()
     {
         return view('admin.RerollCategory.AddRerollCategory');
     }
 
-    public function addRerollCategory(Request $request)
+    public function store(Request $request)
     {
         $request->validate([
             'name' => 'required',
@@ -34,34 +37,57 @@ class RerollCategoryController extends Controller
             'note' => 'required',
         ]);
 
-        $this->rerollCategoryService->add($request->name, $request->image, $request->note);
-        return redirect(route('admin.RerollCategory.index'))->with('success', 'Thêm danh mục thành công');
+        $imagePath = null;
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '_' . $image->getClientOriginalName();
+            $image->move(public_path('image/rerollCategory'), $imageName);
+            $imagePath = 'image/rerollCategory/' . $imageName;
+        }
+
+        RerollCategory::create([
+            'name' => $request->name,
+            'image' => $imagePath,
+            'note' => $request->note,
+        ]);
+
+        return redirect(route('admin.rerollCategory.index'))->with('success', 'Thêm danh mục thành công');
     }
 
-    public function showEditRerollCategory(Request $request)
+    public function edit($id)
     {
-        $id = $request->id;
-        $rerollCategoryInfo = $this->rerollCategoryService->getById($id);
-        return view('admin.RerollCategory.EditRerollCategory', compact('id', 'rerollCategoryInfo'));
+        $rerollCategory = RerollCategory::find($id);
+        return view('admin.RerollCategory.EditRerollCategory', compact('id', 'rerollCategory'));
     }
 
-    public function detailRerollCategory(Request $request)
-    {
-        $id = $request->id;
-        $allRerollSubCategory = $this->rerollCategoryService->getChildren($id);
-        return view('admin.RerollSubCategory.RerollSubCategory', compact('allRerollSubCategory'));
-    }
-
-    public function editRerollCategory(Request $request)
+    public function update($id, Request $request)
     {
         $request->validate([
-            'id' => 'required',
             'name' => 'required',
             'note' => 'required'
         ]);
 
-        $this->rerollCategoryService->edit($request->id, $request->name, $request->note);
-        return redirect(route('admin.RerollCategory.index'))->with('success', 'Sửa danh mục thành công');
+        $rerollCategory = RerollCategory::find($id);
+
+        $imagePath = $rerollCategory->image;
+        if ($request->hasFile('image')) {
+            if (file_exists($imagePath)) {
+                unlink($imagePath);
+            }
+
+            $image = $request->file('image');
+            $imageName = time() . '_' . $image->getClientOriginalName();
+            $image->move(public_path('image/rerollCategory'), $imageName);
+            $imagePath = 'image/rerollCategory/' . $imageName;
+        }
+
+        $rerollCategory->update([
+            'name' => $request->name,
+            'image' => $imagePath,
+            'note' => $request->note,
+        ]);
+
+        return redirect(route('admin.rerollCategory.index'))->with('success', 'Sửa danh mục thành công');
     }
 
     public function ChangeCategoryStatus(Request $request)
@@ -70,12 +96,12 @@ class RerollCategoryController extends Controller
         $rerollCategoryInfo = $this->rerollCategoryService->getById($id);
         if ($rerollCategoryInfo->status == 0) {
             $this->rerollCategoryService->ChangeStatus($id, 1);
-            return redirect(route('admin.RerollCategory.index'))->with('success', 'Hiện danh mục thành công');
+            return redirect()->back()->with('success', 'Hiện danh mục thành công');
         } else if (!$this->rerollCategoryService->checkHasChildren($id)) {
             $this->rerollCategoryService->ChangeStatus($id, 0);
-            return redirect(route('admin.RerollCategory.index'))->with('success', 'Ẩn danh mục thành công');
+            return redirect()->back()->with('success', 'Ẩn danh mục thành công');
         } else {
-            return redirect(route('admin.RerollCategory.index'))->with('error', 'Danh mục đang có sản phẩm không thể Ẩn');
+            return redirect()->back()->with('error', 'Danh mục đang có sản phẩm không thể Ẩn');
         }
     }
 }
