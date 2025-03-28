613d04969f47e0d77e5ca9638abb23cbc5952774
nhat191024 <richberchannel01@gmail.com>
2025-03-28 13:55:43 +0700

    fix: update game recharge routes for consistency and clarity

--- routes/admin/game/recharge.php
+++ routes/admin/game/recharge.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\GameRechargeController;
 
-Route::prefix('/game-recharge')->name('GameRecharge.')->group(function () {
+Route::prefix('/game-recharge')->name('gameRecharge.')->group(function () {
     Route::get('/', [GameRechargeController::class, 'index'])->name('index');
-    Route::get('/add', [GameRechargeController::class, 'showAddRecharge'])->name('showAdd');
-    Route::post('/add', [GameRechargeController::class, 'addRecharge'])->name('add');
-    Route::post('/edit', [GameRechargeController::class, 'editRecharge'])->name('edit');
-    Route::get('/edit/{id}', [GameRechargeController::class, 'showEditRecharge'])->name('showEdit');
-    Route::get('/ChangeStatus/{id}/{status}', [GameRechargeController::class, 'ChangeGameStatus'])->name('ChangeGameRechargeStatus');
+    Route::get('/create', [GameRechargeController::class, 'create'])->name('create');
+    Route::post('/store', [GameRechargeController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [GameRechargeController::class, 'edit'])->name('edit');
+    Route::post('/update/{id}', [GameRechargeController::class, 'update'])->name('update');
+    Route::get('/changeStatus/{id}/{status}', [GameRechargeController::class, 'changeStatus'])->name('changeStatus');
 });
