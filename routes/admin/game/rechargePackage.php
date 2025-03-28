0992e1afe6d59500410c1b040dbe9d23819a6dc3
nhat191024 <richberchannel01@gmail.com>
2025-03-28 14:52:53 +0700

    refactor: update game recharge package routes for improved naming and structure

--- routes/admin/game/rechargePackage.php
+++ routes/admin/game/rechargePackage.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\GameRechargePackageController;
 
-Route::prefix('/game-recharge-package')->name('GameRechargePackage.')->group(function () {
-    Route::get('/list/{id}', [GameRechargePackageController::class, 'index'])->name('index');
-    Route::get('/add', [GameRechargePackageController::class, 'showAddGameRechargePackage'])->name('showAdd');
-    Route::post('/add', [GameRechargePackageController::class, 'addRechargePackage'])->name('add');
-    Route::post('/edit', [GameRechargePackageController::class, 'editRechargePackage'])->name('edit');
-    Route::get('/edit/{id}', [GameRechargePackageController::class, 'showEditRechargePackage'])->name('showEdit');
-    Route::get('/ChangeStatus/{id}/{status}', [GameRechargePackageController::class, 'ChangeGameRechargePackageStatus'])->name('ChangeGameRechargePackageStatus');
+Route::prefix('/game-recharge-package')->name('gameRechargePackage.')->group(function () {
+    Route::get('/{id}', [GameRechargePackageController::class, 'index'])->name('index');
+    Route::get('/create/form', [GameRechargePackageController::class, 'create'])->name('create');
+    Route::post('/store', [GameRechargePackageController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [GameRechargePackageController::class, 'edit'])->name('edit');
+    Route::post('/update/{id}', [GameRechargePackageController::class, 'update'])->name('update');
+    Route::get('/destroy/{id}/{status}', [GameRechargePackageController::class, 'destroy'])->name('destroy');
 });
