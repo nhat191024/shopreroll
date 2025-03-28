066526eefbdba334d2d5ff2671e11cdb0f16a1a5
nhat191024 <richberchannel01@gmail.com>
2025-03-28 14:53:17 +0700

    fix: update GameRechargePackage view for improved naming and data presentation

--- resources/views/admin/gameRechargePackage/GameRechargePackage.blade.php
+++ resources/views/admin/gameRechargePackage/GameRechargePackage.blade.php
@@ -1,10 +1,10 @@
 @extends('admin.master')
 @section('main')
     <div class="container-fluid">
-        <h1 class="h3 mb-2 text-gray-800">Gói nạp {{ $gameRechargeName }}</h1>
+        <h1 class="h3 mb-2 text-gray-800">Gói nạp - {{ $rechargeName }}</h1>
         <div class="card mb-4 shadow">
             <div class="card-header py-3">
-                <a class="btn btn-primary" href="{{ route('admin.GameRechargePackage.showAdd') }}">Thêm gói nạp mới</a>
+                <a class="btn btn-primary" href="{{ route('admin.gameRechargePackage.create', $id) }}">Thêm gói nạp cho {{ $rechargeName }}</a>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
@@ -28,6 +28,27 @@
                                 <th>Chức năng</th>
                             </tr>
                         </thead>
+                        <tbody>
+                            @php
+                                $key = 0;
+                            @endphp
+                            @foreach ($rechargePackages as $item)
+                                <tr>
+                                    <td>{{ ++$key }}</td>
+                                    <td>{{ $item->name }}</td>
+                                    <td>{{ number_format($item->price) }} VNĐ</td>
+                                    <td>{{ $item->status == 1 ? 'Hiện' : 'Ẩn' }}</td>
+                                    <td class="text-center">
+                                        <a class="btn btn-warning" href="{{ route('admin.gameRechargePackage.edit', $item->id) }}">Sửa</a>
+                                        @if ($item->status == 0)
+                                            <a class="btn btn-success" onclick="event.preventDefault(); if (confirm('Bạn chắc chắn muốn hiện item {{ $item->name }} chứ?')) { window.location.href = '{{ route('admin.gameRechargePackage.destroy', [$item->id, 1]) }}'; }">Hiện</a>
+                                        @else
+                                            <a class="btn btn-danger" onclick="event.preventDefault(); if (confirm('Bạn chắc chắn muốn ẩn item {{ $item->name }} chứ?')) { window.location.href = '{{ route('admin.gameRechargePackage.destroy', [$item->id, 0]) }}'; }">Ẩn</a>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
                         <tfoot>
                             <tr>
                                 <th>STT</th>
@@ -37,35 +58,9 @@
                                 <th>Chức năng</th>
                             </tr>
                         </tfoot>
-                        <tbody>
-                            @php
-                                $key = 0;
-                            @endphp
-                            @foreach ($rechargePackages as $item)
-                                <tr>
-                                    <td>{{ ++$key }}</td>
-                                    <td>{{ $item->name }}</td>
-                                    <td>{{ $item->price }}</td>
-                                    <td>{{ $item->status == 1 ? 'Hoạt động' : 'Đã ẩn' }}</td>
-                                    <td class="text-center">
-                                        <a class="btn btn-warning" href="{{ route('admin.GameRechargePackage.showEdit', ['id' => $item->id]) }}">
-                                            Sửa
-                                        </a>
-                                        @if ($item->status == 0)
-                                            <a class="btn btn-success" onclick="event.preventDefault(); if (confirm('Bạn chắc chắn muốn hiện item {{ $item->name }} chứ?')) { window.location.href = '{{ route('admin.GameRechargePackage.ChangeGameRechargePackageStatus', [$item->id, 1]) }}'; }">
-                                                Hiện </a>
-                                        @else
-                                            <a class="btn btn-danger" onclick="event.preventDefault(); if (confirm('Bạn chắc chắn muốn ẩn item {{ $item->name }} chứ?')) { window.location.href = '{{ route('admin.GameRechargePackage.ChangeGameRechargePackageStatus', [$item->id, 0]) }}'; }">
-                                                Ẩn </a>
-                                        @endif
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
                     </table>
                 </div>
             </div>
         </div>
-
     </div>
 @endsection
