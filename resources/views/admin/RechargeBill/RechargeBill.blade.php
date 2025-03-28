a91bb12e02dc378093fe3ac7cee967f4d807682c
nhat191024 <richberchannel01@gmail.com>
2025-03-28 15:09:02 +0700

    fix: restore tfoot section in RechargeBill view for improved table structure

--- resources/views/admin/RechargeBill/RechargeBill.blade.php
+++ resources/views/admin/RechargeBill/RechargeBill.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.master')
 @section('main')
     <div class="container-fluid">
-        <h1 class="h3 mb-2 text-gray-800">Hóa đơn</h1>>
+        <h1 class="h3 mb-2 text-gray-800">Hóa đơn</h1>
         <div class="card mb-4 shadow">
             <div class="card-body">
                 <div class="table-responsive">
@@ -19,6 +19,22 @@
                                 <th>Trạng thái</th>
                             </tr>
                         </thead>
+                        </tfoot>
+                        <tbody>
+                            @foreach ($allRechargeBill as $index => $bill)
+                                <tr>
+                                    <td>{{ $index + 1 }}</td>
+                                    <td>{{ $bill->UID }}</td>
+                                    <td>{{ $bill->User ? $bill->User->name : 'N/A' }}</td>
+                                    <td>{{ $bill->username }}</td>
+                                    <td>{{ $bill->server }}</td>
+                                    <td>{{ $bill->character_name }}</td>
+                                    <td>{{ $bill->RechargePackage->name }}</td>
+                                    <td>{{ number_format($bill->RechargePackage->price) }} VNĐ</td>
+                                    <td>{{ $bill->status == 1 ? 'Đã Thanh Toán' : 'Chưa Thanh Toán' }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
                         <tfoot>
                             <tr>
                                 <th>STT</th>
@@ -31,24 +47,7 @@
                                 <th>Giá</th>
                                 <th>Trạng thái</th>
                             </tr>
-                        </tfoot>
-                        <tbody>
-                            @foreach ($allRechargeBill as $index => $bill)
-                                <tr>
-                                    <td>{{ $index + 1 }}</td>
-                                    <td>{{ $bill->UID }}</td>
-                                    <td>{{ $bill->User ? $bill->User->name : 'N/A' }}</td>
-                                    <td>{{ $bill->username }}</td>
-                                    <td>{{ $bill->server }}</td>
-                                    <td>{{ $bill->character_name }}</td>
-                                    <td>{{ $bill->RechargePackage->name }}</td>
-                                    <td>{{ $bill->RechargePackage->price }}</td>
-                                    <td>{{ $bill->status == 1 ? 'Đã Thanh Toán' : 'Chưa Thanh Toán' }}</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
                     </table>
-
                 </div>
             </div>
         </div>
