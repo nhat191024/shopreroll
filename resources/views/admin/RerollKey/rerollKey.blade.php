1169efe01aa843623f8ab64c04a3fe117b75b72b
nhat191024 <richberchannel01@gmail.com>
2025-03-28 11:49:14 +0700

    fix: update RerollKey view header to conditionally display package name

--- resources/views/admin/RerollKey/rerollKey.blade.php
+++ resources/views/admin/RerollKey/rerollKey.blade.php
@@ -1,7 +1,11 @@
 @extends('admin.master')
 @section('main')
     <div class="container-fluid">
-        <h1 class="h3 mb-2 text-gray-800">Danh sách Reroll Key - {{ $packageName }}</h1>
+        @if (isset($packageName))
+            <h1 class="h3 mb-2 text-gray-800">Danh sách Reroll Key</h1>
+        @else
+            <h1 class="h3 mb-2 text-gray-800">Danh sách Reroll Key - {{ $packageName }}</h1>
+        @endif
         <div class="card mb-4 shadow">
             <div class="card-header py-3">
                 <a class="btn btn-primary" href="{{ route('admin.rerollSubCategory.index', $idPackage) }}">Quay lại</a>
