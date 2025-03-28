6ded15327d328a021e8e9558a91bebf09739086c
nhat191024 <richberchannel01@gmail.com>
2025-03-28 15:19:47 +0700

    fix: simplify BalanceRechargeCardBillController by removing service dependency and directly loading User relationship

--- app/Http/Controllers/admin/BalanceRechargeCardBillController.php
+++ app/Http/Controllers/admin/BalanceRechargeCardBillController.php
@@ -2,22 +2,15 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Models\BalanceRechargeCardBill;
+
 use App\Http\Controllers\Controller;
-use App\Service\admin\BalanceRechargeCardBillService;
-use Illuminate\Http\Request;
 
 class BalanceRechargeCardBillController extends Controller
 {
-    private $balanceRechargeCardBillService;
-
-    public function __construct()
-    {
-        $this->balanceRechargeCardBillService = app(BalanceRechargeCardBillService::class);
-    }
-
     public function index()
     {
-        $allBalanceRechargeCardBill = $this->balanceRechargeCardBillService->getAll();
+        $allBalanceRechargeCardBill = BalanceRechargeCardBill::all()->load('User');
         return view('admin.BalanceRechargeCardBill.BalanceRechargeCardBill', compact('allBalanceRechargeCardBill'));
     }
 }
