a8fcecee4b76e9834402b78667b0fa7a84b1f6a4
Catty <ducvu73@gmail.com>
2025-03-30 17:26:35 +0700

    feat: enhance accessibility features for user profile page

--- resources/views/client/home/home.blade.php
+++ resources/views/client/home/home.blade.php
@@ -1,11 +1,12 @@
 @extends('client.layouts.master')
 
 @section('main')
-<link rel="stylesheet" href="{{ asset('css/home-custom.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/home-custom.css') }}">
     <section class="content">
         <div class="container-fluid">
             <div class="text-center">
-                <img class="city__icon" src="https://uploadstatic-sea.mihoyo.com/contentweb/20200319/2020031919242255224.png" onerror="this.src='https://placehold.co/600x600'" loading="lazy">
+                <img class="city__icon" src="https://uploadstatic-sea.mihoyo.com/contentweb/20200319/2020031919242255224.png"
+                    onerror="this.src='https://placehold.co/600x600'" loading="lazy">
             </div>
             <h1 class="guide__title">Shop bán acc Honkai Star Rail và Genshin uy tín hàng đầu Việt Nam</h1>
             <main>
@@ -14,16 +15,21 @@
                         <div class="row mb-3">
                             <div class="col text-center">
                                 <span class="mt-2">
-                                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
+                                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true"
+                                        aria-expanded="false">
                                         <button class="btn-pretty">Hỗ Trợ</button>
                                     </a>
                                     <div class="dropdown-menu">
                                         <a class="dropdown-item" target="_blank" href="https://zalo.me/0386496488">
-                                            <img src="{{ asset('/image/thumb/zalo.png') }}" onerror="this.src='https://placehold.co/600x600'" alt="zalo" style="max-width: 40px; height: auto; margin-bottom: -8px; margin-top: -8px;" loading="lazy">0386496488
+                                            <img src="{{ asset('/image/thumb/zalo.png') }}"
+                                                onerror="this.src='https://placehold.co/600x600'" alt="zalo"
+                                                style="max-width: 40px; height: auto; margin-bottom: -8px; margin-top: -8px;"
+                                                loading="lazy">0386496488
                                         </a>
                                         <div class="dropdown-divider"></div>
                                         <a class="dropdown-item" target="_blank" href="https://www.facebook.com/dat.ds.3">
-                                            <span style="background-color: #1877f2; border-radius: 4px; padding: 4px 8px 4px 8px;">
+                                            <span
+                                                style="background-color: #1877f2; border-radius: 4px; padding: 4px 8px 4px 8px;">
                                                 <i class="fab fa-facebook-f fa-lg" style="color: white"></i>
                                             </span> Facebook
                                         </a>
@@ -31,15 +37,18 @@
                                 </span>
 
                                 <span class="mt-2">
-                                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
+                                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-haspopup="true"
+                                        aria-expanded="false">
                                         <button class="btn-pretty">Nạp Tiền</button>
                                     </a>
                                     <div class="dropdown-menu">
-                                        <a class="dropdown-item" target="_blank" href="https://shopreroll.com/user/money/phone-card/send-card">
+                                        <a class="dropdown-item" target="_blank"
+                                            href="https://shopreroll.com/user/money/phone-card/send-card">
                                             <i class="fas fa-money-check-alt mr-1"></i> Nạp Thẻ
                                         </a>
                                         <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" target="_blank" href="https://shopreroll.com/user/money/auto-bank/info">
+                                        <a class="dropdown-item" target="_blank"
+                                            href="https://shopreroll.com/user/money/auto-bank/info">
                                             <i class="fas fa-university mr-1"></i> Chuyển Khoản
                                         </a>
                                     </div>
@@ -56,33 +65,43 @@
                             <div id="miu-carousel" class="col-12 col-lg-8 carousel slide p-0" data-ride="carousel">
                                 <div class="carousel-inner">
                                     <div class="carousel-item active" style="margin-bottom: -5px;">
-                                        <iframe width="100%" onerror="this.src='https://placehold.co/600x600'" height="470" src="https://www.youtube.com/embed/LmmfXWOvSU0?si=vxNIin1ImvaFpweu" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        <iframe width="100%" onerror="this.src='https://placehold.co/600x600'"
+                                            height="470"
+                                            src="https://www.youtube.com/embed/LmmfXWOvSU0?si=vxNIin1ImvaFpweu"
+                                            frameborder="0"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                            allowfullscreen></iframe>
                                     </div>
                                 </div>
                             </div>
                             <div id="top_user" class="col-12 col-lg-4 p-0">
                                 <ul id="MiuTopTab" class="nav nav-tabs mb-2" role="tablist">
                                     <li class="nav-item col" role="presentation">
-                                        <a id="top-tab" class="nav-link text-uppercase text-center" data-bs-toggle="tab" href="#toptab" role="tab" aria-controls="profile" aria-selected="false">
+                                        <a id="top-tab" class="nav-link text-uppercase text-center" data-bs-toggle="tab"
+                                            href="#toptab" role="tab" aria-controls="profile" aria-selected="false">
                                             Top nạp tiền
                                         </a>
                                     </li>
                                 </ul>
                                 <div id="nav-user" class="tab-content">
-                                    <div id="toptab" class="tab-pane fade show active" role="tabpanel" aria-labelledby="toptab">
+                                    <div id="toptab" class="tab-pane fade show active" role="tabpanel"
+                                        aria-labelledby="toptab">
                                         <ul class="nav nav-user">
                                             @foreach ([['name' => 'Keith sierra', 'amount' => '22,707,000'], ['name' => 'Nam', 'amount' => '13,282,600'], ['name' => 'Perawit', 'amount' => '11,111,275'], ['name' => 'cau vang', 'amount' => '6,250,000'], ['name' => 'Nguyễn Duy', 'amount' => '4,373,010'], ['name' => 'Vo phuc khang', 'amount' => '4,310,000'], ['name' => 'Dương Quang Ánh', 'amount' => '4,270,000'], ['name' => 'bac', 'amount' => '4,218,520'], ['name' => 'Nguyễn minh Quang', 'amount' => '3,940,002'], ['name' => 'Nguyễn Văn Trường', 'amount' => '3,573,000']] as $index => $user)
                                                 <li class="nav-link w-100" style="padding: .1rem 1rem">
                                                     <div class="row">
-                                                        <div class="col-7 text-left" style="text-align: left; padding-left: 30px;">
+                                                        <div class="col-7 text-left"
+                                                            style="text-align: left; padding-left: 30px;">
                                                             <span class="fa-stack">
                                                                 <span class="fa fa-circle fa-stack-2x"></span>
-                                                                <strong class="fa-stack-1x" style="color: #000;">{{ $index + 1 }}</strong>
+                                                                <strong class="fa-stack-1x"
+                                                                    style="color: #000;">{{ $index + 1 }}</strong>
                                                             </span>
                                                             {{ $user['name'] }}
                                                         </div>
                                                         <div class="col-5">
-                                                            <label class="btn btn-warning float-right mr-4" style="background-color:#ffeaaa; padding: .145rem .7rem; font-weight: 600;">
+                                                            <label class="btn btn-warning float-right mr-4"
+                                                                style="background-color:#ffeaaa; padding: .145rem .7rem; font-weight: 600;">
                                                                 {{ $user['amount'] }}<sup></sup>
                                                             </label>
                                                         </div>
@@ -91,7 +110,8 @@
                                             @endforeach
                                         </ul>
                                     </div>
-                                    <div id="anoumentTab" class="tab-pane fade" role="tabpanel" aria-labelledby="bounustab" style="padding: 10px;">
+                                    <div id="anoumentTab" class="tab-pane fade" role="tabpanel"
+                                        aria-labelledby="bounustab" style="padding: 10px;">
                                     </div>
                                 </div>
                             </div>
@@ -107,9 +127,8 @@
                                             <a href="#">
                                                 <div class="square-image-container">
                                                     <img src="{{ asset($rerollCategory->image) }}"
-                                                         onerror="this.src='https://placehold.co/300x300'"
-                                                         class="square-image"
-                                                         loading="lazy">
+                                                        onerror="this.src='https://placehold.co/300x300'"
+                                                        class="square-image" loading="lazy">
                                                 </div>
                                             </a>
                                         </div>
@@ -128,53 +147,59 @@
                     </div>
 
                     @foreach ($gameAccountCategories as $game)
-                    @if ($game->GameCategory->count() == 0)
-                        @continue
-                    @endif
-                    <center>
-                        <img class="city__icon" src="https://uploadstatic-sea.mihoyo.com/contentweb/20210717/2021071716211547763.png" onerror="this.src='https://placehold.co/600x600'" loading="lazy">
-                    </center>
-                    <h2 id="recharge_service" class="guide__title">Acc {{ $game->name }}</h2>
+                        @if ($game->GameCategory->count() == 0)
+                            @continue
+                        @endif
+                        <center>
+                            <img class="city__icon"
+                                src="https://uploadstatic-sea.mihoyo.com/contentweb/20210717/2021071716211547763.png"
+                                onerror="this.src='https://placehold.co/600x600'" loading="lazy">
+                        </center>
+                        <h2 id="recharge_service" class="guide__title">Acc {{ $game->name }}</h2>
 
-                    <div class="container-lg">
-                        <div class="row justify-content-center">
-                            @foreach ($game->GameCategory as $category)
-                                <article class="col-12 col-sm-6 col-lg-3 item-bounder">
-                                    <center>
-                                        <a href="{{ route('client.recharge', $category->id) }}">
-                                            <div class="game-img-outer">
-                                                <div class="game-img-box">
-                                                    <img class="game-img-content" src="{{ asset($category->image) }}"
-                                                         onerror="this.src='https://placehold.co/600x600'" loading="lazy">
+                        <div class="container-lg">
+                            <div class="row justify-content-center">
+                                @foreach ($game->GameCategory as $category)
+                                    <article class="col-12 col-sm-6 col-lg-3 item-bounder">
+                                        <center>
+                                            <a href="{{ route('client.recharge', $category->id) }}">
+                                                <div class="game-img-outer">
+                                                    <div class="game-img-box">
+                                                        <img class="game-img-content" src="{{ asset($category->image) }}"
+                                                            onerror="this.src='https://placehold.co/600x600'"
+                                                            loading="lazy">
+                                                    </div>
                                                 </div>
+                                            </a>
+                                            <h2 class="note__title">{{ $category->name }}</h2>
+                                            <div class="row g-0 info-line">
+                                                <section class="row g-0 text-center">
+                                                    <label class="text-muted">Số tài khoản</label>
+                                                    <span
+                                                        class="more-detail fs-4">{{ $category->GameAccount->count() }}</span>
+                                                </section>
                                             </div>
-                                        </a>
-                                        <h2 class="note__title">{{ $category->name }}</h2>
-                                        <div class="row g-0 info-line">
-                                            <section class="row g-0 text-center">
-                                                <label class="text-muted">Số tài khoản</label>
-                                                <span class="more-detail fs-4">{{ $category->GameAccount->count() }}</span>
-                                            </section>
-                                        </div>
-                                        <div class="row g-0 info-line">
-                                            <section class="row g-0 text-center">
-                                                <label class="text-muted">Đã bán</label>
-                                                <span class="more-detail fs-4">{{ '0' }}</span>
-                                            </section>
-                                        </div>
-                                        <a href="{{ route('client.game.account.category', ['gameAccountId' => $game->id, 'categoryId' => $category->id]) }}">
-                                            <button class="btn-pretty mb-4 mt-2">Khám phá</button>
-                                        </a>
-                                    </center>
-                                </article>
-                            @endforeach
+                                            <div class="row g-0 info-line">
+                                                <section class="row g-0 text-center">
+                                                    <label class="text-muted">Đã bán</label>
+                                                    <span class="more-detail fs-4">{{ '0' }}</span>
+                                                </section>
+                                            </div>
+                                            <a
+                                                href="{{ route('client.game.account.category', ['gameAccountId' => $game->id, 'categoryId' => $category->id]) }}">
+                                                <button class="btn-pretty mb-4 mt-2">Khám phá</button>
+                                            </a>
+                                        </center>
+                                    </article>
+                                @endforeach
+                            </div>
                         </div>
-                    </div>
-
                     @endforeach
 
                     <center>
-                        <img class="city__icon" src="https://uploadstatic-sea.mihoyo.com/contentweb/20210717/2021071716211547763.png" onerror="this.src='https://placehold.co/600x600'" loading="lazy">
+                        <img class="city__icon"
+                            src="https://uploadstatic-sea.mihoyo.com/contentweb/20210717/2021071716211547763.png"
+                            onerror="this.src='https://placehold.co/600x600'" loading="lazy">
                     </center>
                     <h2 id="recharge_service" class="guide__title">NẠP GAMES</h2>
 
@@ -187,7 +212,7 @@
                                             <div class="game-img-outer">
                                                 <div class="game-img-box">
                                                     <img class="game-img-content" src="{{ asset($gameRecharge->image) }}"
-                                                         onerror="this.src='https://placehold.co/600x600'" loading="lazy">
+                                                        onerror="this.src='https://placehold.co/600x600'" loading="lazy">
                                                 </div>
                                             </div>
                                         </a>
