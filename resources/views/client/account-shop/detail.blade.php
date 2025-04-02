ae3519d1ef962b52c34ec5448f904e42cc5d63b5
nhat191024 <richberchannel01@gmail.com>
2025-04-02 22:04:29 +0700

    style: improve HTML structure and formatting in account shop detail view

--- resources/views/client/account-shop/detail.blade.php
+++ resources/views/client/account-shop/detail.blade.php
@@ -3,9 +3,8 @@
 @section('main')
     <section class="content">
         <div class="container-fluid">
-            <div class="text-center mb-4">
-                <img src="https://uploadstatic-sea.mihoyo.com/contentweb/20200319/2020031919242255224.png" class="city__icon"
-                    alt="City Icon">
+            <div class="mb-4 text-center">
+                <img class="city__icon" src="https://uploadstatic-sea.mihoyo.com/contentweb/20200319/2020031919242255224.png" alt="City Icon">
             </div>
             <main class="pb-5">
                 <div class="container">
@@ -13,7 +12,7 @@
                     <div class="info-detail-product">
                         <div class="row info-line">
                             @foreach ($accountAttributes as $data)
-                                <section class="col-md-4 col-12 text-center text-uppercase">
+                                <section class="col-md-4 col-12 text-uppercase text-center">
                                     <h4>{{ $data->GameAttribute->name }}</h4>
                                     <span>{{ $data->value }}</span>
                                 </section>
@@ -34,10 +33,7 @@
                                     <h4 class="pb-2">{{ $data['type']->name }}</h4>
                                     @foreach ($data['items'] as $itemData)
                                         <span class="hero-details">
-                                            <i class="hero-icon-detail"
-                                                style="background-image: url('{{ '/' . $itemData->image ?? 'null' }}')"
-                                                data-toggle="tooltip" onerror="this.src='https://placehold.co/600x300'"
-                                                title="{{ $itemData->name }}"></i>
+                                            <i class="hero-icon-detail" data-toggle="tooltip" style="background-image: url('{{ '/' . $itemData->image ?? 'null' }}')" onerror="this.src='https://placehold.co/600x300'" title="{{ $itemData->name }}"></i>
                                         </span>
                                     @endforeach
                                 </section>
@@ -46,13 +42,11 @@
                     </div>
                     <div class="row justify-content-center mb-4">
                         <div class="col-md-6 col-12 text-center">
-                            <a href="https://www.youtube.com/feed/trending" target="_blank"
-                                class="btn btn-pretty-detail my-3">
+                            <a class="btn btn-pretty-detail my-3" href="https://www.youtube.com/feed/trending" target="_blank">
                                 Xem Video
                             </a>
-                            <a href="{{ route('client.account-shop.buy-now', ['id' => $gameAccount->id]) }}"
-                                class="btn btn-pretty-detail my-3">
-                                Mua Ngay ({{ $gameAccount->price_out }} VND)
+                            <a class="btn btn-pretty-detail my-3" href="{{ route('client.account-shop.buy-now', ['id' => $gameAccount->id]) }}">
+                                Mua Ngay ({{ number_format($gameAccount->price_out) }} VND)
                             </a>
                         </div>
                     </div>
@@ -64,8 +58,7 @@
                                         <h1 class="title-shine h2 text-center">Hình ảnh chi tiết acc</h1>
                                         <p class="title-shine text-center">{{ $gameAccount->title }}</p>
                                     @endonce
-                                    <img src="{{ asset($data->image) }}" class="product-image" alt="Ảnh acc Genshin"
-                                        onerror="this.src='https://placehold.co/600x300'" />
+                                    <img class="product-image" src="{{ asset($data->image) }}" alt="Ảnh acc Genshin" onerror="this.src='https://placehold.co/600x300'" />
                                 </div>
                             @endforeach
                         </div>
