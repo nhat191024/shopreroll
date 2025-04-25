a9117beec451148b61b6f57d1b3259e92ca31b86
nhat191024 <richberchannel01@gmail.com>
2025-04-26 05:05:35 +0700

    fix: update entrypoint script to set permissions for bootstrap directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,10 @@
 mkdir -p /var/www/html/storage/logs
 touch /var/www/html/storage/logs/laravel.log
 chmod -R 775 /var/www/html/storage
+chmod -R 775 /var/www/html/bootstrap
 chmod -R 775 /var/www/html/bootstrap/cache
 chown -R www-data:www-data /var/www/html/storage
+chown -R www-data:www-data /var/www/html/bootstrap
 chown -R www-data:www-data /var/www/html/bootstrap/cache
 
 exec "$@"
