92dc944a16dd6c3885c62bc8a117f16ece3d1ccb
nhat191024 <richberchannel01@gmail.com>
2025-04-26 05:03:27 +0700

    fix: update entrypoint script to use correct paths for storage and logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Đảm bảo thư mục log tồn tại và có quyền ghi
-mkdir -p /var/www/storage/logs
-touch /var/www/storage/logs/laravel.log
-chmod -R 775 /var/www/storage
-chmod -R 775 /var/www/bootstrap/cache
-chown -R www-data:www-data /var/www/storage
-chown -R www-data:www-data /var/www/bootstrap/cache
+mkdir -p /var/www/html/storage/logs
+touch /var/www/html/storage/logs/laravel.log
+chmod -R 775 /var/www/html/storage
+chmod -R 775 /var/www/html/bootstrap/cache
+chown -R www-data:www-data /var/www/html/storage
+chown -R www-data:www-data /var/www/html/bootstrap/cache
 
 exec "$@"
