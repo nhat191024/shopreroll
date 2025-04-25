f4998d68fc6e06a0e9b5515731596fe713a08b93
nhat191024 <richberchannel01@gmail.com>
2025-04-26 05:17:26 +0700

    fix: ensure framework cache directory exists and set permissions for storage and bootstrap

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Đảm bảo thư mục log tồn tại và có quyền ghi
-# mkdir -p /var/www/html/storage/logs
-# touch /var/www/html/storage/logs/laravel.log
+# Đảm bảo thư mục framework cache tồn tại
+mkdir -p /var/www/html/storage/framework/cache
+
 chmod -R 775 /var/www/html/storage
 chmod -R 775 /var/www/html/bootstrap
 chmod -R 775 /var/www/html/bootstrap/cache
 chown -R www-data:www-data /var/www/html/storage
 chown -R www-data:www-data /var/www/html/bootstrap
 chown -R www-data:www-data /var/www/html/bootstrap/cache
-
-php artisan optimize:clear
 
 exec "$@"
