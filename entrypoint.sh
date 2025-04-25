95abcddc8e691a0b906ba8885f5971349528875b
nhat191024 <richberchannel01@gmail.com>
2025-04-26 05:10:25 +0700

    fix: add php artisan optimize:clear command to entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,6 @@ chown -R www-data:www-data /var/www/html/storage
 chown -R www-data:www-data /var/www/html/bootstrap
 chown -R www-data:www-data /var/www/html/bootstrap/cache
 
+php artisan optimize:clear
+
 exec "$@"
