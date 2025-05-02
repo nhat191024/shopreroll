d95a173508e70e7512c5e57e34c1e00efa762e5c
nhat191024 <richberchannel01@gmail.com>
2025-05-02 22:35:21 +0700

    refactor: move registration logic from RegisterService to RegisterController

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,23 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\UserRegisterRequest as UserRegisterRequest;
-use App\Service\RegisterService;
-use Illuminate\Http\Request;
+use App\Http\Requests\UserRegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    private $registerService;
+    private $FORM_TITLE = 'Đăng ký';
+    private $SUCCESS_MESSAGE = 'Đăng ký tài khoản thành công, vui lòng đăng nhập';
+    private $SUCCESS_REDIRECT = '/login';
 
-    public function __construct(RegisterService $registerService){
-        $this->registerService = $registerService;
-    }
-    public function index(){
-        return $this->registerService->index();
-    }
-
-    public function register(UserRegisterRequest $request){
-        return $this->registerService->register($request);
+    public function index()
+    {
+        if (Auth::check()) {
+            return redirect()->intended('/');
+        }
+        return view('client.auth.register')->with('title', $this->FORM_TITLE);
     }
 
+    public function register(UserRegisterRequest $request)
+    {
+        $name = $request->input('name');
+        $username = $request->input('username');
+        $phone = $request->input('phone');
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $user = new User();
+        $user->name = $name;
+        $user->username = $username;
+        $user->phone = $phone;
+        $user->email = $email;
+        $user->password = Hash::make($password);
+        $user->save();
+
+        return redirect($this->SUCCESS_REDIRECT)->with('message', $this->SUCCESS_MESSAGE);
+    }
 }
