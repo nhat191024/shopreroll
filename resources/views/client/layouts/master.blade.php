deca8a3ce567a61be5dc5747d55bbb9bb4d89c12
nhat191024 <richberchannel01@gmail.com>
2025-05-02 22:46:45 +0700

    refactor: improve HTML formatting for consistency and readability

--- resources/views/client/layouts/master.blade.php
+++ resources/views/client/layouts/master.blade.php
@@ -7,10 +7,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description"
-        content="Mua bán tài khoản Honkai Star Rail, Shop acc Honkai Star Rail VIP, Reroll uy tín hàng đầu Việt Nam">
-    <meta name="keywords"
-        content="Shop acc Honkai Star Rail VIP,Mua bán tài khoản Honkai Star Rail,Mua Acc Genshin Giá Rẻ Nhất,reroll" />
+    <meta name="description" content="Mua bán tài khoản Honkai Star Rail, Shop acc Honkai Star Rail VIP, Reroll uy tín hàng đầu Việt Nam">
+    <meta name="keywords" content="Shop acc Honkai Star Rail VIP,Mua bán tài khoản Honkai Star Rail,Mua Acc Genshin Giá Rẻ Nhất,reroll" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta property='og:image' content='https://img.upanh.tv/2023/05/17/12312312312-01-01.png' />
 
@@ -21,15 +19,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-        media="print" onload="this.media='all'">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" media="print" onload="this.media='all'">
 
     <!-- Bootstrap 4 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 
     <!-- Theme style -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1.0/dist/css/adminlte.min.css" media="print"
-        onload="this.media='all'">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1.0/dist/css/adminlte.min.css" media="print" onload="this.media='all'">
 
     <!-- DataTables CSS -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
@@ -58,8 +54,7 @@
     <nav class="main-header navbar navbar-expand-md nav-header mb-4">
         <div class="container">
 
-            <button class="navbar-toggler order-1" data-toggle="collapse" data-target="#navbarCollapse" type="button"
-                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler order-1" data-toggle="collapse" data-target="#navbarCollapse" type="button" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon">
                     <i class="fas fa-bars" style="text-shadow: 2px 2px 2px #000000;color: #fff;"></i>
                 </span>
@@ -70,8 +65,7 @@
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link" href="/">
-                            <img class="img-fluid" src="https://img.upanh.tv/2023/05/17/12312312312-01-01.png"
-                                style="margin-top: -8px;height: 165%">
+                            <img class="img-fluid" src="https://img.upanh.tv/2023/05/17/12312312312-01-01.png" style="margin-top: -8px;height: 165%">
                         </a>
                     </li>
                     <li class="nav-item">
@@ -80,15 +74,12 @@
                         </a>
                     </li>
                     <li id="topUp_balance" class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle menu-header" data-toggle="dropdown" href="#"
-                            aria-haspopup="true" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle menu-header" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
                             Nạp tiền
                         </a>
                         <ul class="dropdown-menu border-0 shadow" style="left: 0px; right: inherit;">
                             <li>
-                                <a class="dropdown-item"
-                                    href="{{ route('client.user.topup.card') }}"><i
-                                        class="fas fa-money-check-alt mr-1"></i>
+                                <a class="dropdown-item" href="{{ route('client.user.topup.card') }}"><i class="fas fa-money-check-alt mr-1"></i>
                                     Nạp bằng thẻ cào
                                 </a>
                             </li>
@@ -107,8 +98,7 @@
                         </a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a data-toggle="dropdown" href="#" aria-haspopup="true"
-                            aria-expanded="false"class="nav-link dropdown-toggle menu-header">
+                        <a data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false"class="nav-link dropdown-toggle menu-header">
                             Lịch Sử Mua
                         </a>
                         <ul class="dropdown-menu border-0 shadow" style="left: 0px; right: inherit;">
@@ -152,9 +142,7 @@
             <ul class="order-md-3 navbar-nav navbar-no-expand order-1 ml-auto" style="position: absolute;right: 0px;">
                 @if (Auth::check())
                     <li class="nav-item mr-3">
-                        <span class="dropdown-toggle btn btn-block btn-outline-warning font-weight-bold"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                            style="background: rgb(89 84 173 / 25%);">
+                        <span class="dropdown-toggle btn btn-block btn-outline-warning font-weight-bold" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="background: rgb(89 84 173 / 25%);">
                             <span class="text-light">[{{ Auth::user()->id }}] {{ Auth::user()->name }}:</span>
                             {{ number_format(Auth::user()->balance) }}đ<sup></sup>
                         </span>
@@ -169,25 +157,21 @@
                                     @endif
                                 </span></li>
                             <li class="dropdown-divider"></li>
-                            <li><a class="dropdown-item text-dark" href="{{ route('client.user.balance-history') }}"><i
-                                        class="fas fa-history mr-1"></i> Biến động số dư
+                            <li><a class="dropdown-item text-dark" href="{{ route('client.user.balance-history') }}"><i class="fas fa-history mr-1"></i> Biến động số dư
                                 </a></li>
                             <li class="dropdown-divider"></li>
-                            <li><a class="dropdown-item text-dark" href="{{ '' }}"><i
-                                        class="fas fa-history mr-1"></i> Lịch sử khác
+                            <li><a class="dropdown-item text-dark" href="{{ '' }}"><i class="fas fa-history mr-1"></i> Lịch sử khác
                                 </a></li>
                             <li class="dropdown-divider"></li>
-                            <li><a class="dropdown-item text-dark" href="{{ route('client.user.change') }}"><i
-                                        class="fas fa-key mr-1"></i> Đổi mật khẩu
+                            <li><a class="dropdown-item text-dark" href="{{ route('client.user.change') }}"><i class="fas fa-key mr-1"></i> Đổi mật khẩu
                                 </a></li>
                             <li class="dropdown-divider"></li>
-                            <li><a class="dropdown-item text-dark" href="{{ route('logout') }}"><i
-                                        class="fa fa-dungeon mr-1"></i> Đăng xuất
+                            <li><a class="dropdown-item text-dark" href="{{ route('logout') }}"><i class="fa fa-dungeon mr-1"></i> Đăng xuất
                                 </a></li>
                         </ul>
                     </li>
                 @else
-                    <li class="nav-item mr-3 d-none d-md-block">
+                    <li class="nav-item d-none d-md-block mr-3">
                         <a href="{{ route('register') }}">
                             <button class="btn-pretty">Đăng ký</button>
                         </a>
@@ -208,8 +192,7 @@
         <div class="container-lg">
             <h2 class="guide__title mt-3">
                 <a href="/">
-                    <img src="https://img.upanh.tv/2023/05/17/12312312312-01-01.png"
-                        style="margin-top: -8px;height: 45px">
+                    <img src="https://img.upanh.tv/2023/05/17/12312312312-01-01.png" style="margin-top: -8px;height: 45px">
                 </a>
             </h2>
             <div class="row">
@@ -262,8 +245,7 @@
     </div>
     <ul class="nav-fixed">
         <li class="nav-fixed-zalo">
-            <a target="_blank" href="https://zalo.me/0386496488"><img
-                    src="{{ asset('/image/thumb/zalo.png') }}"></a>
+            <a target="_blank" href="https://zalo.me/0386496488"><img src="{{ asset('/image/thumb/zalo.png') }}"></a>
         </li>
         <li class="nav-fixed-face">
             <a target="_blank" href="https://www.facebook.com/dat.ds.3">
@@ -278,10 +260,7 @@
     </ul>
 
     <!-- DataTables JS -->
-    <script
-        src="https://cdn.datatables.net/v/bs4/jq-3.7.0/dt-2.2.2/b-3.2.2/b-colvis-3.2.2/r-3.0.4/sl-3.0.0/datatables.min.js"
-        integrity="sha384-B8hhapBzyENLm121fc/+Itc1gDWTHRWHm+vNGfDZ3TF2jQEBhrdZRssP/CQ8Og+r" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.datatables.net/v/bs4/jq-3.7.0/dt-2.2.2/b-3.2.2/b-colvis-3.2.2/r-3.0.4/sl-3.0.0/datatables.min.js" integrity="sha384-B8hhapBzyENLm121fc/+Itc1gDWTHRWHm+vNGfDZ3TF2jQEBhrdZRssP/CQ8Og+r" crossorigin="anonymous"></script>
 
     <!-- Additional DataTable Button Scripts -->
     <script src="https://cdn.datatables.net/buttons/3.2.2/js/dataTables.buttons.min.js"></script>
