af2f881b6075cf606a5a532f1ca315559a736c2d
nhat191024 <richberchannel01@gmail.com>
2025-05-05 02:46:30 +0700

    refactor: simplify user edit form and enhance input validation

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -1,63 +1,67 @@
 @extends('admin.master')
 @section('main')
     <div class="container-fluid">
+        <h1 class="h3 mb-2 text-gray-800">Cập nhật thông tin tài khoản</h1>
         <div class="card mb-4 shadow">
             <div class="card-body">
-                <div class="row">
-                    {{-- <form action="{{ route('admin.user.edit', ['id' => $user->id]) }}" method="post" enctype="multipart/form-data"> --}}
-                    <form action="{{ route('admin.user.edit', $user->id) }}" method="post" enctype="multipart/form-data">
-
-                        @csrf
-                        <h1 class="h3 mb-2 text-gray-800">Cập nhật thông tin </h1>
-                        <div class="row">
-                            <div class="form-group col-6">
-                                <label for="">ID tài khoản</label>
-                                <input class="form-control" name="id" type="text" required value="{{ $user['id'] }}" readonly>
-                            </div>
-                            <div class="form-group col-6">
-                                <label for="">Cộng tiền (Nhập số âm để trừ tiền)</label>
-                                <input class="form-control" name="addedBalance" type="number" value="0" placeholder="Nhập số tiền muốn thêm ">
-                            </div>
-                            <div class="form-group col-6">
-                                <label for="">Họ và tên</label>
-                                <input class="form-control" name="name" type="text" required value="{{ $user['name'] }}" readonly>
-                            </div>
-                            <div class="form-group col-6">
-                                <label for="">Quyền hạn </label>
-                                <select class="form-control" name="role" required>
-                                    <option value="" disabled {{ $user->role === null ? 'selected' : '' }}>Select Role</option>
-                                    <option value="1" {{ $user->role == 1 ? 'selected' : '' }}>Quản trị viên</option>
-                                    <option value="0" {{ $user->role == 0 ? 'selected' : '' }}>Khách Hàng</option>
-                                    <option value="3" {{ $user->role == 3 ? 'selected' : '' }}>Bị chặn</option>
-                                </select>
-                            </div>
-                            <div class="form-group col-6">
-                                <label for="">Số dư hiện tại</label>
-                                <input class="form-control" name="" type="text" required value="{{ number_format($user['balance'], 0, ',', '.') }} VND" readonly>
-                                <input name="balance" type="hidden" value="{{ $user['balance'] }}">
-                            </div>
-                            <div class="form-group col-6">
-                                <label for="">tài khoản người dùng</label>
-                                <input class="form-control" name="username" type="text" required value="{{ $user['username'] }}" readonly>
-                            </div>
-                            <div class="form-group col-6">
-                                <label for="">E-mail tài khoản</label>
-                                <input class="form-control" name="email" type="email" required value="{{ $user['email'] }}" readonly>
-                            </div>
-                            <div class="form-group col-6">
-                                <label for="">Số điện thoại tài khoản</label>
-                                <input class="form-control" name="phone" type="number" required value="{{ $user['phone'] }}" readonly>
-                            </div>
-                            <div class="form-group col-12">
-                                <h1 class="h3 mb-2 text-gray-800">Thay đổi mật khẩu </h1>
-                                <label for="">Mật khẩu mới của tài khoản</label>
-                                <input class="form-control" name="password" type="password" placeholder="Nhập mật khẩu mới">
-                            </div>
-                        </div>
-                        <input name="id" type="hidden" value="{{ $user->id }}">
-                        <button class="btn btn-primary mt-4" name="action" type="submit" value="update">Cập nhật thông tin</button>
-                    </form>
-                </div>
+                <form action="{{ route('admin.user.edit', $user->id) }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group">
+                        <label for="">Tên đăng nhập</label>
+                        <input class="form-control" name="username" type="text" value="{{ $user['username'] }}" readonly>
+                        @error('username')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="">Mật khẩu mới</label>
+                        <input class="form-control" name="password" type="text" placeholder="Nhập mật khẩu mới">
+                        @error('password')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="">Tên người dùng</label>
+                        <input class="form-control" name="name" type="text" value="{{ $user['name'] }}" disabled>
+                        @error('name')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="">Vai trò </label>
+                        <select class="form-control" name="role" aria-label="Default select example">
+                            <option value="" disabled>Chọn Vai trò </option>
+                            <option value="1" {{ $user->role == 1 ? 'selected' : '' }}>Quản trị viên</option>
+                            <option value="0" {{ $user->role == 0 ? 'selected' : '' }}>Khách Hàng</option>
+                            <option value="3" {{ $user->role == 3 ? 'selected' : '' }}>Bị chặn</option>
+                        </select>
+                        @error('role')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="">Số dư </label>
+                        <input class="form-control" name="balance" type="number" value="{{ $user['balance'] }}" placeholder="Nhập số dư">
+                        @error('balance')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="">E-mail</label>
+                        <input class="form-control" name="email" type="email" value="{{ $user['email'] }}" disabled>
+                        @error('email')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="">Số điện thoại</label>
+                        <input class="form-control" name="phone" type="text" value="{{ $user['phone'] }}" disabled>
+                        @error('phone')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <button class="btn btn-primary mt-4" type="submit">Cập nhật</button>
+                </form>
             </div>
         </div>
     </div>
