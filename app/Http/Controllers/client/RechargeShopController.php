c54143152ea59936a1c8858554d2675c3570d66d
Catty <ducvu73@gmail.com>
2025-05-05 05:36:51 +0700

    refactor: update recharge confirmation to use consistent variable naming and add success message

--- app/Http/Controllers/client/RechargeShopController.php
+++ app/Http/Controllers/client/RechargeShopController.php
@@ -31,7 +31,9 @@ class RechargeShopController extends Controller
         if (!auth()->check()) {
             return redirect()->route('login');
         }
-        $recharge_packet_id = $this->rechargeShopService->rechargeConfirm($request);
-        return redirect()->route('client.recharge', ['id' => $recharge_packet_id]);
+        $game_recharge_id = $this->rechargeShopService->rechargeConfirm($request);
+
+        return redirect()->route('client.recharge', ['id' => $game_recharge_id])
+            ->with('success', 'Đã gửi yêu cầu nạp!');;
     }
 }
