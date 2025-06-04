a278311a8de14a93a8647130ce088be2745e2ab2
mlemingcapoo <ducvu73@gmail.com>
2025-06-04 18:14:18 +0700

    update: add session message alerts to account detail page

--- resources/views/client/account-shop/detail.blade.php
+++ resources/views/client/account-shop/detail.blade.php
@@ -2,6 +2,21 @@
 
 @section('main')
     <section class="content">
+        @if (session()->has('error'))
+            <marquee direction="right" behavior="alternate" class="alert alert-danger">
+                <i class="fa fa-quote-left"></i> {{ session('error') }}
+            </marquee>
+        @endif
+        @if (session()->has('message'))
+            <marquee direction="right" behavior="alternate" class="alert alert-info">
+                <i class="fa fa-quote-left"></i> {{ session('message') }}
+            </marquee>
+        @endif
+        @if (session()->has('success'))
+            <marquee direction="right" behavior="alternate" class="alert alert-success">
+                <i class="fa fa-quote-left"></i> {{ session('success') }}
+            </marquee>
+        @endif
         <div class="container-fluid">
             <div class="mb-4 text-center">
                 <img class="city__icon" src="https://uploadstatic-sea.mihoyo.com/contentweb/20200319/2020031919242255224.png" alt="City Icon">
